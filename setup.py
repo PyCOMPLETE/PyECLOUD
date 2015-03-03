74efab687bb5367d2aaaf78222827b6a0470b6db
like2000 <kevin.li.cern@gmail.com>
2015-03-03 14:38:42 +0100

    add cross platform compatibility numpy-cython

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
+import numpy
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize([Extension("boris_cython", ["boris_cython.pyx", 'boris_c_function.c']), 
-								Extension("geom_impact_poly_cython", ["geom_impact_poly_cython.pyx"])]
-								, annotate=True)
-)
+ ext_modules = cythonize([Extension("boris_cython", ["boris_cython.pyx", 'boris_c_function.c'],
+                                include_dirs=[numpy.get_include()]),
+                             Extension("geom_impact_poly_cython", ["geom_impact_poly_cython.pyx"],
+                                include_dirs=[numpy.get_include()], annotate=True)]))
+
