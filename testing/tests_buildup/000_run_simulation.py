aef7ec4d0fe00052f2c1c54c283e9705316b6d23
giadarol <giovanni.iadarola@cern.ch>
2016-01-26 14:23:03 +0100

    Preparing quadrupole test

--- testing/tests_buildup/000_run_simulation.py
+++ testing/tests_buildup/000_run_simulation.py
@@ -2,8 +2,8 @@ import sys, os
 BIN = os.path.expanduser("../../../")
 sys.path.append(BIN)
 
-sim_folder = 'LHC_ArcDipReal_450GeV_sey1.60_2.5e11ppb_bl_1.00ns'
-# sim_folder = 'LHC_ArcQuadReal_450GeV_sey1.65_2.5e11ppb_bl_1.00ns'
+#~ sim_folder = 'LHC_ArcDipReal_450GeV_sey1.60_2.5e11ppb_bl_1.00ns'
+sim_folder = 'LHC_ArcQuadReal_450GeV_sey1.65_2.5e11ppb_bl_1.00ns'
 
 
 from PyECLOUD.buildup_simulation import BuildupSimulation
