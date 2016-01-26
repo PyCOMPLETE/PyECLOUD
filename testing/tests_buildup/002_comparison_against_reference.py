53c8ae8f20ddb1060ade83c63dd6fb7bc482ffad
giadarol <giovanni.iadarola@cern.ch>
2016-01-26 16:28:30 +0100

    very minor change

--- testing/tests_buildup/002_comparison_against_reference.py
+++ testing/tests_buildup/002_comparison_against_reference.py
@@ -14,7 +14,8 @@ import PyECLOUD.mystyle as ms
 
 pl.close('all')
 
-sim_folder = 'LHC_ArcDipReal_450GeV_sey1.60_2.5e11ppb_bl_1.00ns'
+sim_folder = 'LHC_ArcQuadReal_450GeV_sey1.60_2.5e11ppb_bl_1.00ns'
+sim_folder = 'LHC_ArcQuadReal_450GeV_sey1.65_2.5e11ppb_bl_1.00ns'
 
 ref_folder = sim_folder
 curr_folder = sim_folder
