0371faf2df0f1158159d7b96bec1dfea0bd4219d
giadarol <giovanni.iadarola@cern.ch>
2016-02-08 13:00:10 +0100

    reinitialize as a separate member

--- PyEC4PyHT.py
+++ PyEC4PyHT.py
@@ -254,51 +254,9 @@ class Ecloud(object):
 			if self.N_tracks>0:
 				print 'Warning: Track skipped because track_only_first_time is True.'
 				return
-		
-		#reinitialize
-		self.MP_e.x_mp[:self.init_N_mp] = self.init_x #it is a mutation and not a binding (and we have tested it :-))
-		self.MP_e.y_mp[:self.init_N_mp] = self.init_y
-		self.MP_e.z_mp[:self.init_N_mp] = self.init_z
-		self.MP_e.vx_mp[:self.init_N_mp] = self.init_vx
-		self.MP_e.vy_mp[:self.init_N_mp] = self.init_vy
-		self.MP_e.vz_mp[:self.init_N_mp] = self.init_vz
-		self.MP_e.nel_mp[:self.init_N_mp] = self.init_nel
-		self.MP_e.N_mp = self.init_N_mp
-		
-		MP_e = self.MP_e
-		dynamics = self.dynamics
-		impact_man = self.impact_man
-		spacech_ele = self.spacech_ele
-
-		if self.save_ele_distributions_last_track:
-			self.rho_ele_last_track = []
-
-		if self.save_ele_potential_and_field:
-			self.save_ele_potential = True
-			self.save_ele_field = True
+				
+		self._reinitialize()
 			
-		if self.save_ele_potential:
-			self.phi_ele_last_track = []
-			
-		if self.save_ele_field:
-			self.Ex_ele_last_track = []
-			self.Ey_ele_last_track = []
-
-		if self.save_ele_MP_position:
-			self.x_MP_last_track = []
-			self.y_MP_last_track = []
-
-		if self.save_ele_MP_velocity:
-			self.vx_MP_last_track = []
-			self.vy_MP_last_track = []
-
-		if self.save_ele_MP_size:
-			self.nel_MP_last_track = []
-			
-		if self.save_ele_MP_position or self.save_ele_MP_velocity or self.save_ele_MP_size:
-			self.N_MP_last_track = []
-			
-		
 		if hasattr(beam.particlenumber_per_mp, '__iter__'):
 			raise ValueError('ecloud module assumes same size for all beam MPs')
 
@@ -314,83 +272,9 @@ class Ecloud(object):
 
 			# slice size and time step
 			dz = (slices.z_bins[i + 1] - slices.z_bins[i])
-			dt = dz / (beam.beta * c)
 			
-			# define substep
-			if dt>self.Dt_ref:
-				N_sub_steps = int(np.round(dt/self.Dt_ref))
-			else:
-				N_sub_steps=1
-				
-			Dt_substep = dt/N_sub_steps
-			#print Dt_substep, N_sub_steps, dt
+			self._track_single_slice(beam, ix, dz)
 
-			# beam field 
-			MP_p = MP_light()
-			MP_p.x_mp = beam.x[ix]+self.x_beam_offset
-			MP_p.y_mp = beam.y[ix]+self.y_beam_offset
-			MP_p.nel_mp = beam.x[ix]*0.+beam.particlenumber_per_mp/dz#they have to become cylinders
-			MP_p.N_mp = slices.n_macroparticles_per_slice[i]
-			#compute beam field (it assumes electrons!)
-			spacech_ele.recompute_spchg_efield(MP_p)
-			#scatter to electrons
-			Ex_n_beam, Ey_n_beam = spacech_ele.get_sc_eletric_field(MP_e)
-			# go to actual beam particles 
-			Ex_n_beam = -Ex_n_beam * beam.charge/e
-			Ey_n_beam = -Ey_n_beam * beam.charge/e
-			
-			
-			## compute electron field map
-			spacech_ele.recompute_spchg_efield(MP_e)
-			
-			## compute electron field on electrons
-			Ex_sc_n, Ey_sc_n = spacech_ele.get_sc_eletric_field(MP_e)
-			
-			## compute electron field on beam particles
-			Ex_sc_p, Ey_sc_p = spacech_ele.get_sc_eletric_field(MP_p)
-			
-			## Total electric field on electrons
-			Ex_n=Ex_sc_n+Ex_n_beam;
-			Ey_n=Ey_sc_n+Ey_n_beam;
-				
-			## save position before motion step
-			old_pos=MP_e.get_positions()
-			
-			## motion electrons
-			MP_e = dynamics.stepcustomDt(MP_e, Ex_n,Ey_n, Dt_substep=Dt_substep, N_sub_steps=N_sub_steps)
-			
-			## impacts: backtracking and secondary emission
-			MP_e = impact_man.backtrack_and_second_emiss(old_pos, MP_e)
-			
-			## kick beam particles
-			fact_kick = beam.charge/(beam.mass*beam.beta*beam.beta*beam.gamma*c*c)*self.L_ecloud
-			beam.xp[ix]+=fact_kick*Ex_sc_p
-			beam.yp[ix]+=fact_kick*Ey_sc_p
-			
-			if self.save_ele_distributions_last_track:
-				self.rho_ele_last_track.append(spacech_ele.rho.copy())
-				#print 'Here'
-
-			if self.save_ele_potential:
-				self.phi_ele_last_track.append(spacech_ele.phi.copy())
-			
-			if self.save_ele_field:
-				self.Ex_ele_last_track.append(spacech_ele.efx.copy())
-				self.Ey_ele_last_track.append(spacech_ele.efy.copy())
-
-			if self.save_ele_MP_position:
-				self.x_MP_last_track.append(MP_e.x_mp.copy())
-				self.y_MP_last_track.append(MP_e.y_mp.copy())
-
-			if self.save_ele_MP_velocity:
-				self.vx_MP_last_track.append(MP_e.vx_mp.copy())
-				self.vy_MP_last_track.append(MP_e.vy_mp.copy())
-				
-			if self.save_ele_MP_size:
-				self.nel_MP_last_track.append(MP_e.nel_mp.copy())
-				
-			if self.save_ele_MP_position or self.save_ele_MP_velocity or self.save_ele_MP_size:
-				self.N_MP_last_track.append(MP_e.N_mp)
 				
 		if self.save_ele_distributions_last_track:
 			self.rho_ele_last_track = np.array(self.rho_ele_last_track[::-1])
@@ -457,6 +341,136 @@ class Ecloud(object):
 		self.track_only_first_time = False
 		self.replace_with_recorded_field_map(delete_ecloud_data=delete_ecloud_data)
 		
+			
+	def _track_single_slice(self, beam, ix, dz):
+		
+		MP_e = self.MP_e
+		dynamics = self.dynamics
+		impact_man = self.impact_man
+		spacech_ele = self.spacech_ele
+			
+		dt = dz / (beam.beta * c)
+		
+		# define substep
+		if dt>self.Dt_ref:
+			N_sub_steps = int(np.round(dt/self.Dt_ref))
+		else:
+			N_sub_steps=1
+			
+		Dt_substep = dt/N_sub_steps
+		#print Dt_substep, N_sub_steps, dt
+
+		# beam field 
+		MP_p = MP_light()
+		MP_p.x_mp = beam.x[ix]+self.x_beam_offset
+		MP_p.y_mp = beam.y[ix]+self.y_beam_offset
+		MP_p.nel_mp = beam.x[ix]*0.+beam.particlenumber_per_mp/dz#they have to become cylinders
+		MP_p.N_mp = len(beam.x[ix])
+		#compute beam field (it assumes electrons!)
+		spacech_ele.recompute_spchg_efield(MP_p)
+		#scatter to electrons
+		Ex_n_beam, Ey_n_beam = spacech_ele.get_sc_eletric_field(MP_e)
+		# go to actual beam particles 
+		Ex_n_beam = -Ex_n_beam * beam.charge/e
+		Ey_n_beam = -Ey_n_beam * beam.charge/e
+		
+		
+		## compute electron field map
+		spacech_ele.recompute_spchg_efield(MP_e)
+		
+		## compute electron field on electrons
+		Ex_sc_n, Ey_sc_n = spacech_ele.get_sc_eletric_field(MP_e)
+		
+		## compute electron field on beam particles
+		Ex_sc_p, Ey_sc_p = spacech_ele.get_sc_eletric_field(MP_p)
+		
+		## Total electric field on electrons
+		Ex_n=Ex_sc_n+Ex_n_beam;
+		Ey_n=Ey_sc_n+Ey_n_beam;
+			
+		## save position before motion step
+		old_pos=MP_e.get_positions()
+		
+		## motion electrons
+		MP_e = dynamics.stepcustomDt(MP_e, Ex_n,Ey_n, Dt_substep=Dt_substep, N_sub_steps=N_sub_steps)
+		
+		## impacts: backtracking and secondary emission
+		MP_e = impact_man.backtrack_and_second_emiss(old_pos, MP_e)
+		
+		## kick beam particles
+		fact_kick = beam.charge/(beam.mass*beam.beta*beam.beta*beam.gamma*c*c)*self.L_ecloud
+		beam.xp[ix]+=fact_kick*Ex_sc_p
+		beam.yp[ix]+=fact_kick*Ey_sc_p
+		
+		if self.save_ele_distributions_last_track:
+			self.rho_ele_last_track.append(spacech_ele.rho.copy())
+			#print 'Here'
+
+		if self.save_ele_potential:
+			self.phi_ele_last_track.append(spacech_ele.phi.copy())
+		
+		if self.save_ele_field:
+			self.Ex_ele_last_track.append(spacech_ele.efx.copy())
+			self.Ey_ele_last_track.append(spacech_ele.efy.copy())
+
+		if self.save_ele_MP_position:
+			self.x_MP_last_track.append(MP_e.x_mp.copy())
+			self.y_MP_last_track.append(MP_e.y_mp.copy())
+
+		if self.save_ele_MP_velocity:
+			self.vx_MP_last_track.append(MP_e.vx_mp.copy())
+			self.vy_MP_last_track.append(MP_e.vy_mp.copy())
+			
+		if self.save_ele_MP_size:
+			self.nel_MP_last_track.append(MP_e.nel_mp.copy())
+			
+		if self.save_ele_MP_position or self.save_ele_MP_velocity or self.save_ele_MP_size:
+			self.N_MP_last_track.append(MP_e.N_mp)
+
+	def _reinitialize(self):
+		
+		self.MP_e.x_mp[:self.init_N_mp] = self.init_x #it is a mutation and not a binding (and we have tested it :-))
+		self.MP_e.y_mp[:self.init_N_mp] = self.init_y
+		self.MP_e.z_mp[:self.init_N_mp] = self.init_z
+		self.MP_e.vx_mp[:self.init_N_mp] = self.init_vx
+		self.MP_e.vy_mp[:self.init_N_mp] = self.init_vy
+		self.MP_e.vz_mp[:self.init_N_mp] = self.init_vz
+		self.MP_e.nel_mp[:self.init_N_mp] = self.init_nel
+		self.MP_e.N_mp = self.init_N_mp
+		
+		MP_e = self.MP_e
+		dynamics = self.dynamics
+		impact_man = self.impact_man
+		spacech_ele = self.spacech_ele
+
+		if self.save_ele_distributions_last_track:
+			self.rho_ele_last_track = []
+
+		if self.save_ele_potential_and_field:
+			self.save_ele_potential = True
+			self.save_ele_field = True
+			
+		if self.save_ele_potential:
+			self.phi_ele_last_track = []
+			
+		if self.save_ele_field:
+			self.Ex_ele_last_track = []
+			self.Ey_ele_last_track = []
+
+		if self.save_ele_MP_position:
+			self.x_MP_last_track = []
+			self.y_MP_last_track = []
+
+		if self.save_ele_MP_velocity:
+			self.vx_MP_last_track = []
+			self.vy_MP_last_track = []
+
+		if self.save_ele_MP_size:
+			self.nel_MP_last_track = []
+			
+		if self.save_ele_MP_position or self.save_ele_MP_velocity or self.save_ele_MP_size:
+			self.N_MP_last_track = []
+				
 			
 def read_parameter_files_pyhdtl(pyecl_input_folder):
     switch_model=0
