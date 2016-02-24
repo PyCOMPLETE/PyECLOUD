dc79b0d23107357f8d7048e06ebe4d3337d86a1a
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2016-02-24 17:39:30 +0200

    Continue doc

--- doc/example/000_plot_main_output.py
+++ doc/example/000_plot_main_output.py
@@ -99,9 +99,104 @@ pl.suptitle('Var. name: cen_density\nelectron density at the beam position')
 pl.subplots_adjust(top=.82, bottom=.14)
 pl.savefig('fig%02d.png'%ifig, dpi=dpiset)
 
+
+
+
 #The output file also contains variables represented by matrices with dimension (t_hist, xg_hist) where t_hist[i] represent the time
 #right before the i-th passage in the machine and xg_hist[i] represents the position of the i-th slice inside the chamber
 #--> dim(t_hist)=#passages, dim(xg_hist)=#slices 
+
+
+#P |----------------------|
+#A |----------------------|
+#S |----------------------|
+#S |----------------------|
+#A |----------------------|
+#G |----------------------|
+#E |----------------------|
+			#SLICE
+
+#8.
+ifig+=1; pl.figure(ifig)
+pl.plot(ob.t, ob.cen_density)
+pl.xlabel('Time [s]')
+pl.ylabel('$e^-$ density [$m^{-3}$]')
+ms.scix(); pl.grid('on')
+pl.suptitle('Var. name: cen_density\nelectron density at the beam position')
+pl.subplots_adjust(top=.82, bottom=.14)
+pl.savefig('fig%02d.png'%ifig, dpi=dpiset)
+
+#9.
+ifig+=1; pl.figure(ifig)
+pl.plot(np.sum(ob.nel_hist, axis=1)) #axis=1: sum w.r.t. columns
+pl.xlabel('Passage')
+pl.ylabel('Number of $e^-$ per unit length [$m^{-1}$]') 
+ms.scix(); pl.grid('on')
+pl.suptitle('Var. name: nel_hist\nnumber of electrons at each passage')
+pl.subplots_adjust(top=.82, bottom=.14)
+pl.savefig('fig%02d.png'%ifig, dpi=dpiset)
+
+#10.
+ifig+=1; pl.figure(ifig)
+pl.plot(np.sum(ob.nel_hist, axis=0)) #axis=0: sum w.r.t. rows
+pl.xlabel('Chamber slice position [m]') #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+pl.ylabel('Number of $e^-$ per unit length [$m^{-1}$]') 
+ms.scix(); pl.grid('on')
+pl.suptitle('Var. name: nel_hist\nnumber of electrons in each slice')
+pl.subplots_adjust(top=.82, bottom=.14)
+pl.savefig('fig%02d.png'%ifig, dpi=dpiset)
+
+#11.
+ifig+=1; pl.figure(ifig)
+pl.plot(np.sum(ob.nel_impact_hist_scrub, axis=1)) 
+pl.xlabel('Passage')
+pl.ylabel('Number of impacting $e^-$ per passage after scrubbing')
+ms.scix(); pl.grid('on')
+pl.suptitle('Var. name: nel_impact_hist_scrub\nnumber of impacting electrons per passage after scrubbing')
+pl.subplots_adjust(top=.82, bottom=.14)
+pl.savefig('fig%02d.png'%ifig, dpi=dpiset)
+
+#12.
+#~ ifig+=1; pl.figure(ifig)
+#~ pl.plot(ob.xg_hist, np.mean(ob.nel_impact_hist_scrub, axis=0)) 
+#~ pl.xlabel('Chamber slice position [m]')
+#~ pl.ylabel('Average of impacting $e^-$ per unit length [$m^{-1}$]')
+#~ ms.scix(); pl.grid('on')
+#~ pl.suptitle('Var. name: nel_impact_hist_scrub\nmean value of impacting electrons per slice after scrubbing')
+#~ pl.subplots_adjust(top=.82, bottom=.14)
+#~ pl.savefig('fig%02d.png'%ifig, dpi=dpiset)
+
+#13.
+ifig+=1; pl.figure(ifig)
+pl.plot(np.sum(ob.nel_impact_hist_tot, axis=1)) 
+pl.xlabel('Passage')
+pl.ylabel('Number of impacting $e^-$ per unit length [$m^{-1}$]') 
+ms.scix(); pl.grid('on')
+pl.suptitle('Var. name: nel_impact_hist_tot\nnumber of impacting electrons at each passage')
+pl.subplots_adjust(top=.82, bottom=.14)
+pl.savefig('fig%02d.png'%ifig, dpi=dpiset)
+
+#14.
+#~ ifig+=1; pl.figure(ifig)
+#~ pl.plot(ob.xg_hist, np.mean(ob.nel_impact_hist_tot, axis=0)) 
+#~ pl.xlabel('Chamber slice position [m]')
+#~ pl.ylabel('Average of impacting $e^-$ unit length [$m^{-1}$]')
+#~ ms.scix(); pl.grid('on')
+#~ pl.suptitle('Var. name: nel_impact_hist_tot\nmean value of impacting electrons in each slice')
+#~ pl.subplots_adjust(top=.82, bottom=.14)
+#~ pl.savefig('fig%02d.png'%ifig, dpi=dpiset)
+
+#15.
+ifig+=1; pl.figure(ifig)
+pl.plot(ob.N_mp_pass) 
+pl.xlabel('Passage')
+pl.ylabel('Number of MP per unit length [$m^{-1}$]') 
+ms.scix(); pl.grid('on')
+pl.suptitle('Var. name: N_mp_pass\nnumber of MP at each passage')
+pl.subplots_adjust(top=.82, bottom=.14)
+pl.savefig('fig%02d.png'%ifig, dpi=dpiset)
+
+
 
 
 
