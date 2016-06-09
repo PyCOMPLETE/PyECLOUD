d8d784cfc20f0f5a595d02bb641d8e771df7f331
giadarol <giovanni.iadarola@cern.ch>
2016-06-09 12:28:37 +0200

    Status...

--- testing/tests_PyEC4PyHT_multigrid/000_one_pinch.py
+++ testing/tests_PyEC4PyHT_multigrid/000_one_pinch.py
@@ -10,7 +10,7 @@ import PyECLOUD.mystyle as ms
 
 n_segments =1
 machine_configuration = '6.5_TeV_collision_tunes'
-p0_GeV = 2000.
+p0_GeV = 450
 
 z_cut = 2.5e-9*c
 n_slices = 150
@@ -82,8 +82,8 @@ ecloud_multigrid = PyEC4PyHT.Ecloud(
         target_grid = {'x_min_target':-5*bunch.sigma_x(), 'x_max_target':5*bunch.sigma_x(),
                        'y_min_target':-5*bunch.sigma_y(),'y_max_target':5*bunch.sigma_y(),
                        'Dh_target':.2*bunch.sigma_x()},
-        N_nodes_discard = 3.,
-        N_min_Dh_main = 5,
+        N_nodes_discard = 10.,
+        N_min_Dh_main = 10,
         x_beam_offset = x_beam_offset,
         y_beam_offset = y_beam_offset,
         probes_position = probes_position,
@@ -111,53 +111,106 @@ print 'Multigrid tracking time ', t_sw_single /N_rep_test
 
 
                      
-flag_plot_efield_at_probes = True
-if flag_plot_efield_at_probes:
-        
-        slices = bunch.get_slices(ecloud_singlegrid.slicer)
-            
-        pl.close('all')
-        
-        import matplotlib.gridspec as gridspec
-        fig = pl.figure(1, figsize=(7,8))
-        fig.patch.set_facecolor('w')
-        gs1 = gridspec.GridSpec(1, 1)
-        gs2 = gridspec.GridSpec(2, 1)
-        
-        sp1 = fig.add_subplot(gs1[0])
-        obcham =  mlm.myloadmat_to_obj( 'LHC_chm_ver.mat') 	
-        sp1.plot(obcham.Vx*1e3, obcham.Vy*1e3, 'b')
-        sp1.plot(ecloud_singlegrid.x_probes*1e3, ecloud_singlegrid.y_probes*1e3, 'or', markersize=3)
-        sp1.plot(x_beam_offset, y_beam_offset, '*k', markersize=4)
-        sp1.set_ylabel('y [mm]')
-        sp1.set_xlabel('x [mm]')
-        sp1.axis('equal')
-        ms.sciy()
-        ms.scix()
-        sp1.grid('on')
-        
-        
-        sp2 = fig.add_subplot(gs2[0])
-        sp2.plot(slices.z_centers*1e2, ecloud_singlegrid.Ex_ele_last_track_at_probes, 'b.', markersize=6)
-        sp2.plot(slices.z_centers*1e2, ecloud_multigrid.Ex_ele_last_track_at_probes, 'r.', markersize=6)
-        sp2.set_ylabel('Ex at probes [V/m]')
-        sp2.set_xlabel('z [cm]')
-        sp2.grid('on')
-        ms.sciy()
-        
-        sp3 = fig.add_subplot(gs2[1])
-        sp3.plot(slices.z_centers*1e2, ecloud_singlegrid.Ey_ele_last_track_at_probes, 'b.', markersize=6)
-        sp3.plot(slices.z_centers*1e2, ecloud_multigrid.Ey_ele_last_track_at_probes, 'r.', markersize=6)
 
-        sp3.set_ylabel('Ey at probes [V/m]')
-        sp3.set_xlabel('z [cm]')
-        sp3.grid('on')
         
-        ms.sciy()
+slices = bunch.get_slices(ecloud_singlegrid.slicer)
+    
+pl.close('all')
 
-        gs1.tight_layout(fig, rect=[0.25, 0.6, 0.75, 0.98], pad=1.08, h_pad=.9)     #[left, bottom, right, top]
-        gs2.tight_layout(fig, rect=[0, 0, 1, 0.65], pad=1.08, h_pad=.9)     #[left, bottom, right, top]
-            
-        pl.savefig('1.png', dpi=300)
-        pl.show()
-        
+import matplotlib.gridspec as gridspec
+fig = pl.figure(1, figsize=(7,8))
+fig.patch.set_facecolor('w')
+gs1 = gridspec.GridSpec(1, 1)
+gs2 = gridspec.GridSpec(2, 1)
+
+sp1 = fig.add_subplot(gs1[0])
+obcham =  mlm.myloadmat_to_obj( 'LHC_chm_ver.mat') 	
+sp1.plot(obcham.Vx*1e3, obcham.Vy*1e3, 'b')
+sp1.plot(ecloud_singlegrid.x_probes*1e3, ecloud_singlegrid.y_probes*1e3, 'or', markersize=3)
+sp1.plot(x_beam_offset, y_beam_offset, '*k', markersize=4)
+sp1.set_ylabel('y [mm]')
+sp1.set_xlabel('x [mm]')
+sp1.axis('equal')
+ms.sciy()
+ms.scix()
+sp1.grid('on')
+
+
+sp2 = fig.add_subplot(gs2[0])
+sp2.plot(slices.z_centers*1e2, ecloud_singlegrid.Ex_ele_last_track_at_probes, 'b.', markersize=6)
+sp2.plot(slices.z_centers*1e2, ecloud_multigrid.Ex_ele_last_track_at_probes, 'r.', markersize=6)
+sp2.set_ylabel('Ex at probes [V/m]')
+sp2.set_xlabel('z [cm]')
+sp2.grid('on')
+ms.sciy()
+
+sp3 = fig.add_subplot(gs2[1])
+sp3.plot(slices.z_centers*1e2, ecloud_singlegrid.Ey_ele_last_track_at_probes, 'b.', markersize=6)
+sp3.plot(slices.z_centers*1e2, ecloud_multigrid.Ey_ele_last_track_at_probes, 'r.', markersize=6)
+
+sp3.set_ylabel('Ey at probes [V/m]')
+sp3.set_xlabel('z [cm]')
+sp3.grid('on')
+
+ms.sciy()
+
+gs1.tight_layout(fig, rect=[0.25, 0.6, 0.75, 0.98], pad=1.08, h_pad=.9)     #[left, bottom, right, top]
+gs2.tight_layout(fig, rect=[0, 0, 1, 0.65], pad=1.08, h_pad=.9)     #[left, bottom, right, top]
+    
+pl.savefig('1.png', dpi=300)
+
+
+
+########################
+#plot error map
+########################
+pic_singlegrid = ecloud_singlegrid.spacech_ele.PyPICobj
+pic_multigrid = ecloud_multigrid.spacech_ele.PyPICobj
+
+Dh_test = .5e-4
+x_grid_probes = np.arange(np.min(pic_singlegrid.xg), np.max(pic_singlegrid.xg)+Dh_test, Dh_test)
+y_grid_probes = np.arange(np.min(pic_singlegrid.yg), np.max(pic_singlegrid.yg), Dh_test)
+
+[xn, yn]=np.meshgrid(x_grid_probes,y_grid_probes)
+xn=xn.T
+xn=xn.flatten()
+yn=yn.T
+yn=yn.flatten()
+
+#pic gather
+
+Ex_singlegrid_n, Ey_singlegrid_n = pic_singlegrid.gather(xn, yn)	
+Ex_singlegrid_matrix=np.reshape(Ex_singlegrid_n,(len(y_grid_probes),len(x_grid_probes)), 'F').T
+Ey_singlegrid_matrix=np.reshape(Ey_singlegrid_n,(len(y_grid_probes),len(x_grid_probes)), 'F').T
+
+Ex_multigrid_n, Ey_multigrid_n = pic_multigrid.gather(xn, yn)	
+Ex_multigrid_matrix=np.reshape(Ex_multigrid_n,(len(y_grid_probes),len(x_grid_probes)), 'F').T
+Ey_multigrid_matrix=np.reshape(Ey_multigrid_n,(len(y_grid_probes),len(x_grid_probes)), 'F').T
+
+vmin = -7; vmax = -2
+pl.figure(4, figsize=(12, 6)).patch.set_facecolor('w')
+sp1 = pl.subplot(2,2,1)
+pl.pcolormesh(x_grid_probes, y_grid_probes, 
+	np.log10(np.sqrt(Ex_singlegrid_matrix**2+Ey_singlegrid_matrix**2).T), vmin=vmin, vmax=vmax)
+pl.xlabel('x [m]')
+pl.ylabel('y [m]')
+cb=pl.colorbar(); pl.axis('equal')
+cb.formatter.set_powerlimits((0, 0))
+cb.update_ticks()
+cb.set_label('RMS error')
+sp1.ticklabel_format(style='sci', scilimits=(0,0),axis='x') 
+sp1.ticklabel_format(style='sci', scilimits=(0,0),axis='y')
+
+pl.subplot(2,2,2, sharex=sp1, sharey=sp1)
+pl.pcolormesh(x_grid_probes, y_grid_probes, 
+	np.log10(np.sqrt(Ex_multigrid_matrix**2+Ex_multigrid_matrix**2).T), vmin=vmin, vmax=vmax)
+pl.xlabel('x [m]')
+pl.ylabel('y [m]')
+cb=pl.colorbar(); pl.axis('equal')
+cb.formatter.set_powerlimits((0, 0))
+cb.update_ticks()
+cb.set_label('RMS error')
+sp1.ticklabel_format(style='sci', scilimits=(0,0),axis='x') 
+sp1.ticklabel_format(style='sci', scilimits=(0,0),axis='y')
+
+pl.show()
