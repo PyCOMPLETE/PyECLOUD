c5492f6973098920653d5d2aa800dd3ff7e3fd97
giadarol <giovanni.iadarola@cern.ch>
2016-07-25 11:26:22 +0200

    Change version to 5.5.0

--- impact_management_class.py
+++ impact_management_class.py
@@ -7,7 +7,7 @@
 #     
 #     This file is part of the code:
 #                                                                      		            
-#                  PyECLOUD Version 5.4.1                     
+#                  PyECLOUD Version 5.5.0                     
 #                  
 #                                                                       
 #     Author and contact:   Giovanni IADAROLA 
