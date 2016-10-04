deb28da0dd2529cdc66bfdc524deec5f42ff01d6
Philipp Dijkstal <philipp.dijkstal@cern.ch>
2016-10-04 14:25:45 +0200

    Added a consistency test for simulations with photoemission activated. The point (x0_refl, y0_refl) as specified in the input files is checked for being inside the chamber, otherwise an error is raised.

--- gen_photoemission_class.py
+++ gen_photoemission_class.py
@@ -88,6 +88,11 @@ class photoemission:
         if y0_refl!=0.:
             raise ValueError('The case y0_refl!=0 is NOT IMPLEMETED yet!!!!')
         
+        x0_refl_np_arr = np.array([x0_refl])
+        y0_refl_np_arr = np.array([y0_refl])
+        if np.any(self.chamb.is_outside(x0_refl_np_arr, y0_refl_np_arr)):
+            raise ValueError('x0_refl, y0_refl is outside of the chamber!')
+
         print 'Done photoemission init.'
 
     def generate(self, MP_e, lambda_t, Dt):
