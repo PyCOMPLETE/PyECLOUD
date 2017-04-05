519de0c50eaa7074becf343c2820eef66d1d6b0c
Philipp Dijkstal <philipp.dijkstal@cern.ch>
2017-04-05 15:59:36 +0200

    setup.py changed for new c function

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 setup(
- ext_modules = cythonize([Extension("boris_cython", ["boris_cython.pyx", 'boris_c_function.c'],
+ ext_modules = cythonize([Extension("boris_cython", ["boris_cython.pyx", 'boris_c_function.c', 'boris_c_function_simple.c'],
                                 include_dirs=[numpy.get_include()]),
                              Extension("geom_impact_poly_cython", ["geom_impact_poly_cython.pyx"],
                                 include_dirs=[numpy.get_include()], annotate=True)]))
