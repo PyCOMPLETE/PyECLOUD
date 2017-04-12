615ff1121b62bf9350b29166ecb0097afe5cd96d
Philipp Dijkstal <philipp.dijkstal@cern.ch>
2017-04-12 14:55:01 +0200

    minor

--- testing/tests_buildup/tests.py
+++ testing/tests_buildup/tests.py
@@ -6,4 +6,5 @@ all_sim_folders = [
     'LHC_ArcQuadReal_450GeV_sey1.65_2.5e11ppb_bl_1.00ns',
     'LHC_Sextupole_450GeV_sey1.65_2.5e11ppb_bl_1.00ns',
     'LHC_Sextupole_450GeV_sey1.65_2.5e11ppb_bl_1.00ns_skew',
+    'LHC_ArcQuadReal_450GeV_sey1.65_2.5e11ppb_bl_1.00ns_skew',
     ]
