a55ea0823bab80b540480aec7b63023594c66c12
Philipp Dijkstal <philipp.dijkstal@cern.ch>
2017-09-11 19:38:51 +0200

    finalized class photoemission_from_file

--- gen_photoemission_class.py
+++ gen_photoemission_class.py
@@ -50,19 +50,18 @@
 #----------------------------------------------------------------------
 
 from __future__ import division, print_function
+import numpy as np
 import numpy.random as random
-from numpy import interp, pi, sin, cos, zeros, sum
 
 import scipy.io as sio
 import scipy.stats as stats
 from scipy.constants import e as qe, m_e as me, c
-import numpy as np
 
 import sec_emission
 
 qm=qe/me
 
-class photoemission:
+class photoemission(object):
 
     def __init__(self, inv_CDF_refl_photoem_file, k_pe_st, refl_frac, e_pe_sigma, e_pe_max,alimit, x0_refl,
                  y0_refl, out_radius, chamb, resc_fac, energy_distribution, photoelectron_angle_distribution):
@@ -97,28 +96,83 @@ class photoemission:
         if np.any(self.chamb.is_outside(x0_refl_np_arr, y0_refl_np_arr)):
             raise ValueError('x0_refl, y0_refl is outside of the chamber!')
 
-        self.get_energy = get_energy_distribution_func(energy_distribution, e_pe_sigma, e_pe_max)
+        self.get_energy = self.get_energy_distribution_func(energy_distribution, e_pe_sigma, e_pe_max)
         print('Done photoemission init. Energy distribution: %s' % energy_distribution)
+
+    def get_energy_distribution_func(self, energy_distribution, e_pe_sigma, e_pe_max):
+        if energy_distribution == 'lognormal':
+
+            def get_energy(N_int_new_MP):
+                return random.lognormal(e_pe_max, e_pe_sigma, N_int_new_MP)
+
+        elif energy_distribution == 'gaussian':
+
+            def get_energy(N_int_new_MP):
+                En_gen = random.randn(N_int_new_MP)*e_pe_sigma + e_pe_max
+                flag_negat = (En_gen<0.)
+                N_neg = sum(flag_negat)
+                while(N_neg>0):
+                    En_gen[flag_negat] = random.randn(N_neg)*e_pe_sigma + e_pe_max   #in eV
+                    flag_negat = (En_gen<0.)
+                    N_neg = sum(flag_negat)
+                return En_gen
+
+        elif energy_distribution == 'rect':
+
+            def get_energy(N_int_new_MP):
+                return e_pe_max + (random.rand(N_int_new_MP)-0.5)*e_pe_sigma
+
+        elif energy_distribution == 'mono':
+
+            def get_energy(N_int_new_MP):
+                return np.ones(N_int_new_MP)*e_pe_max
+
+        elif energy_distribution == 'lorentz':
+
+            xx_min = stats.cauchy.cdf(0, e_pe_max, e_pe_sigma)
+            xx_max = 1
+
+            def get_energy(N_int_new_MP):
+                xx_rand = random.rand(N_int_new_MP)*(xx_max-xx_min) + xx_min
+                return stats.cauchy.ppf(xx_rand, e_pe_max, e_pe_sigma)
+        else:
+            raise ValueError('Energy distribution not specified')
+
+        return get_energy
+
+    def get_number_new_mps(self, lambda_t, Dt, nel_mp_ref):
+        k_pe = self.k_pe_st*c
+        DNel = k_pe*lambda_t*Dt
+
+        N_new_MP=DNel/nel_mp_ref
+        rest, Nint_new_MP = np.modf(N_new_MP)
+        return int(Nint_new_MP+int(random.rand()<rest))
+
+    def gen_energy_and_set_MPs(self, Nint_new_MP, x_in, y_in, x_out, y_out, MP_e):
+        #generate points and normals
+
+        z_in = z_out = np.zeros_like(x_out)
+
+        x_int, y_int, _, Norm_x, Norm_y, i_found= self.chamb.impact_point_and_normal(
+            x_in, y_in, z_in,x_out, y_out, z_out, resc_fac=self.resc_fac)
+
+        #generate energies (the same distr. for all photoelectr.)
+        En_gen = self.get_energy(Nint_new_MP) #in eV
+        # generate velocities like in impact managment
+        vx_gen, vy_gen, vz_gen = self.angle_dist_func(Nint_new_MP, En_gen, Norm_x, Norm_y)
+
+        MP_e.add_new_MPs(Nint_new_MP, MP_e.nel_mp_ref, x_int, y_int, 0., vx_gen, vy_gen, vz_gen)
 
     def generate(self, MP_e, lambda_t, Dt):
 
-        k_pe=self.k_pe_st*c
-        #determine the number of MPs to be generated
-        DNel=k_pe*lambda_t*Dt
-
-        N_new_MP=DNel/MP_e.nel_mp_ref
-        Nint_new_MP = int(N_new_MP)
-        rest=N_new_MP-Nint_new_MP
-        Nint_new_MP=Nint_new_MP+int(random.rand()<rest)
-        Nint_new_MP=int(Nint_new_MP)
-
+        Nint_new_MP = self.get_number_new_mps(lambda_t, Dt, MP_e.nel_mp_ref)
 
         if Nint_new_MP>0:
             #generate appo x_in and x_out
-            x_in = zeros(Nint_new_MP)
-            y_in = zeros(Nint_new_MP)
-            x_out = zeros(Nint_new_MP)
-            y_out = zeros(Nint_new_MP)
+            x_in = np.zeros(Nint_new_MP)
+            y_in = np.zeros(Nint_new_MP)
+            x_out = np.zeros(Nint_new_MP)
+            y_out = np.zeros(Nint_new_MP)
 
             #for each one generate flag refl
             refl_flag=(random.rand(Nint_new_MP)<self.refl_frac)
@@ -129,75 +183,57 @@ class photoemission:
             if N_refl>0:
                 u_gen=random.rand(N_refl)
                 if self.flag_unif:
-                    psi_gen = 2.*pi*u_gen
-                    x_out[refl_flag]=self.out_radius*cos(psi_gen)
-                    y_out[refl_flag]=self.out_radius*sin(psi_gen)
+                    psi_gen = 2.*np.pi*u_gen
+                    x_out[refl_flag]=self.out_radius*np.cos(psi_gen)
+                    y_out[refl_flag]=self.out_radius*np.sin(psi_gen)
                 else:
-                    psi_gen=interp(u_gen, self.u_sam_CDF_refl, self.inv_CDF_refl)
+                    psi_gen = np.interp(u_gen, self.u_sam_CDF_refl, self.inv_CDF_refl)
                     x_in[refl_flag]=self.x0_refl
-                    x_out[refl_flag]=-2.*self.out_radius*cos(psi_gen)+self.x0_refl
-                    y_out[refl_flag]=2.*self.out_radius*sin(psi_gen)
-
+                    x_out[refl_flag]=-2.*self.out_radius*np.cos(psi_gen)+self.x0_refl
+                    y_out[refl_flag]=2.*self.out_radius*np.sin(psi_gen)
 
             #generate theta for nonreflected photon generation
             N_gauss=np.sum(gauss_flag)
             if N_gauss>0:
                 #theta_gen=self.alimit*randn(N_gauss)
                 theta_gen = random.normal(0, self.alimit, N_gauss)
-                x_out[gauss_flag]=self.out_radius*cos(theta_gen)
-                y_out[gauss_flag]=self.out_radius*sin(theta_gen)
+                x_out[gauss_flag]=self.out_radius*np.cos(theta_gen)
+                y_out[gauss_flag]=self.out_radius*np.sin(theta_gen)
 
-            #generate points and normals
-            x_int, y_int, _, Norm_x, Norm_y, i_found= self.chamb.impact_point_and_normal(x_in, y_in, 0*x_in,
-                                                                                         x_out, y_out, 0*x_out, resc_fac=self.resc_fac)
+            self.gen_energy_and_set_MPs(Nint_new_MP, x_in, y_in, x_out, y_out, MP_e)
 
-            #generate energies (the same distr. for all photoelectr.)
-            En_gen = self.get_energy(Nint_new_MP) #in eV
-            # generate velocities like in impact managment
-            vx_gen, vy_gen, vz_gen = self.angle_dist_func(Nint_new_MP, En_gen, Norm_x, Norm_y)
-
-            MP_e.add_new_MPs(Nint_new_MP, MP_e.nel_mp_ref, x_int, y_int, 0., vx_gen, vy_gen, vz_gen)
+        return MP_e
 
 
-class photoemission_from_file(object):
-    def __init__(self, photoemission_file, energy_distribution, e_pe_sigma, e_pe_max):
+class photoemission_from_file(photoemission):
+    def __init__(self, photoemission_file, chamb, resc_frac, energy_distribution, e_pe_sigma, e_pe_max, k_pe_st, out_radius, photoelectron_angle_distribution):
         print('Start photoemission init from file %s.' % photoemission_file)
-        self.get_energy = get_energy_distribution_func(energy_distribution, e_pe_sigma, e_pe_max)
-        print('Done photoemission init. Energy distribution: %s' % energy_distribution)
+
+        mat = sio.loadmat(photoemission_file)
+        self.inv_CDF = mat['inv_CDF']
+        self.angles = mat['angles']
+        self.k_pe_st = k_pe_st
+        self.out_radius = out_radius
+        self.chamb = chamb
+        self,resc_frac = resc_frac
+
+        self.get_energy = self.get_energy_distribution_func(energy_distribution, e_pe_sigma, e_pe_max)
+        self.angle_dist_func = sec_emission.get_angle_dist_func(photoelectron_angle_distribution)
+        print('Done photoemission init')
 
     def generate(self, MP_e, lambda_t, Dt):
-        pass
 
+        Nint_new_MP = self.get_number_new_mps(lambda_t, Dt, MP_e.nel_mp_ref)
+        if Nint_new_MP>0:
 
-def get_energy_distribution_func(energy_distribution, e_pe_sigma, e_pe_max):
-    if energy_distribution == 'lognormal':
-        def get_energy(N_int_new_MP):
-            return random.lognormal(e_pe_max, e_pe_sigma, N_int_new_MP)
-    elif energy_distribution == 'gaussian':
-        def get_energy(N_int_new_MP):
-            En_gen = random.randn(N_int_new_MP)*e_pe_sigma + e_pe_max
-            flag_negat = (En_gen<0.)
-            N_neg = sum(flag_negat)
-            while(N_neg>0):
-                En_gen[flag_negat] = random.randn(N_neg)*e_pe_sigma + e_pe_max   #in eV
-                flag_negat = (En_gen<0.)
-                N_neg = sum(flag_negat)
-            return En_gen
-    elif energy_distribution == 'rect':
-        def get_energy(N_int_new_MP):
-            return e_pe_max + (random.rand(N_int_new_MP)-0.5)*e_pe_sigma
-    elif energy_distribution == 'mono':
-        def get_energy(N_int_new_MP):
-            return np.ones(N_int_new_MP)*e_pe_max
-    elif energy_distribution == 'lorentz':
-        xx_min = stats.cauchy.cdf(0, e_pe_max, e_pe_sigma)
-        xx_max = 1
+            cdf_gen = random.rand(Nint_new_MP)
+            theta_gen = np.interp(cdf_gen, self.inv_CDF, self.angles)
 
-        def get_energy(N_int_new_MP):
-            xx_rand = random.rand(N_int_new_MP)*(xx_max-xx_min) + xx_min
-            return stats.cauchy.ppf(xx_rand, e_pe_max, e_pe_sigma)
-    else:
-        raise ValueError('Energy distribution not specified')
+            x_out = self.out_radius*np.cos(theta_gen)
+            y_out = self.out_radius*np.sin(theta_gen)
 
-    return get_energy
+            x_in = y_in = np.zeros(Nint_new_MP)
+            self.gen_energy_and_set_MPs(Nint_new_MP, x_in, y_in, x_out, y_out, MP_e)
+
+        return MP_e
 
