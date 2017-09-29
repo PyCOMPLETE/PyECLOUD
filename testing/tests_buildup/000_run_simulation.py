83e2cab147bcf17c7432fee1d6a2f44db2f604bc
lmether <lotta.mether@cern.ch>
2017-09-29 17:47:27 +0200

    Introduced a gas ionization test suite, with ecloud build-up and ion accumulation for the electron and positron CLIC damping rings.
    Conflicts:
    	testing/tests_buildup/000_run_simulation.py
    	testing/tests_buildup/001_comparison_against_reference.py
    	testing/tests_buildup/002_run_all_tests.py
    	testing/tests_buildup/003_update_reference_matfiles.py

--- testing/tests_buildup/000_run_simulation.py
+++ testing/tests_buildup/000_run_simulation.py
@@ -23,6 +23,10 @@ sim_folder = 'LHC_ArcDipReal_450GeV_sey1.70_2.5e11ppb_bl_1.00ns'
 #sim_folder = './LHC_Drift_6500GeV_sey_1.70_1.1e11ppb_b1_1.00ns'
 #sim_folder = 'LHC_ArcDipReal_450GeV_sey1.70_2.5e11ppb_bl_1.00ns_seyfromfile'
 #sim_folder = 'LHC_Triplet_Quadrupole_multiple_beams'
+#sim_folder = 'CLIC_DRe-_Drift_0.5ns_4.0e9ppb_gas_ionization_ecloud_sey2.0'
+#sim_folder = 'CLIC_DRe+_Drift_0.5ns_4.0e9ppb_gas_ionization_ecloud_sey2.0'
+#sim_folder = 'CLIC_DRe-_Drift_0.5ns_4.0e9ppb_gas_ionization_ions_A18'
+#sim_folder = 'CLIC_DRe+_Drift_0.5ns_4.0e9ppb_gas_ionization_ions_A18'
 
 
 # check if user provided folder as command line argument
