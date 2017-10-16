469ba0067fc12b8e1c5268966bec6cb80df49c96
giadarol <giovanni.iadarola@cern.ch>
2017-10-16 18:25:29 +0200

    Generalized buildup init

--- init.py
+++ init.py
@@ -114,9 +114,10 @@ def read_parameter_files(pyecl_input_folder='./', skip_beam_files=False):
 
     return config_dict
 
-def read_input_files_and_init_components(pyecl_input_folder='./', **kwargs):
+def read_input_files_and_init_components(pyecl_input_folder='./', skip_beam=False, 
+            skip_pyeclsaver=False, skip_spacech_ele=False, **kwargs):
 
-    config_dict = read_parameter_files(pyecl_input_folder)
+    config_dict = read_parameter_files(pyecl_input_folder, skip_beam_files = skip_beam)
 
     # Override config values with kwargs
     for attr, value in kwargs.items():
@@ -129,21 +130,24 @@ def read_input_files_and_init_components(pyecl_input_folder='./', **kwargs):
 
     cc = mlm.obj_from_dict(config_dict)
 
-    flag_presence_sec_beams = len(cc.secondary_beams_file_list)>0
-    b_par = pbf.beam_descr_from_fil(os.path.join(pyecl_input_folder, cc.beam_parameters_file), cc.betafx, cc.Dx, cc.betafy, cc.Dy)
+    if not skip_beam:
+        flag_presence_sec_beams = len(cc.secondary_beams_file_list)>0
+        b_par = pbf.beam_descr_from_fil(os.path.join(pyecl_input_folder, cc.beam_parameters_file), cc.betafx, cc.Dx, cc.betafy, cc.Dy)
 
-    sec_b_par_list=[]
-    if flag_presence_sec_beams:
-        for sec_b_file in cc.secondary_beams_file_list:
-            sec_b_par_list.append(pbf.beam_descr_from_fil(os.path.join(pyecl_input_folder, sec_b_file), cc.betafx, cc.Dx, cc.betafy, cc.Dy))
+        sec_b_par_list=[]
+        if flag_presence_sec_beams:
+            for sec_b_file in cc.secondary_beams_file_list:
+                sec_b_par_list.append(pbf.beam_descr_from_fil(os.path.join(pyecl_input_folder, sec_b_file), cc.betafx, cc.Dx, cc.betafy, cc.Dy))
 
-    if cc.B==-1:
-        cc.B = 2*np.pi*b_par.beta_rel*b_par.energy_J/(c*qe*cc.bm_totlen)
+
 
 
     ##########################################
 
-    pyeclsaver=pysav.pyecloud_saver(cc.logfile_path)
+    if not skip_pyeclsaver:
+        pyeclsaver=pysav.pyecloud_saver(cc.logfile_path)
+    else:
+        pyeclsaver = None
 
     if cc.switch_model=='ECLOUD_nunif':
         flag_non_unif_sey = 1
@@ -173,69 +177,93 @@ def read_input_files_and_init_components(pyecl_input_folder='./', **kwargs):
                        cc.Dx_hist, cc.Nx_regen, cc.Ny_regen, cc.Nvx_regen, cc.Nvy_regen, cc.Nvz_regen, cc.regen_hist_cut, chamb,
                        N_mp_soft_regen=cc.N_mp_soft_regen, N_mp_after_soft_regen=cc.N_mp_after_soft_regen)
 
-    beamtim=beatim.beam_and_timing(b_par.flag_bunched_beam, b_par.fact_beam, b_par.coast_dens, b_par.beam_field_file,cc.lam_th,
-                 b_spac=b_par.b_spac, sigmaz=b_par.sigmaz,t_offs=b_par.t_offs, filling_pattern_file=b_par.filling_pattern_file, Dt=cc.Dt, t_end=cc.t_end,
-                 beam_long_prof_file=b_par.beam_long_prof_file, Dh_beam_field=b_par.Dh_beam_field, f_telescope_beam=b_par.f_telescope_beam,
-                 target_grid_beam=b_par.target_grid_beam, N_nodes_discard_beam=b_par.N_nodes_discard_beam, N_min_Dh_main_beam=b_par.N_min_Dh_main_beam,
-                 chamb=chamb,  sigmax=b_par.sigmax, sigmay=b_par.sigmay,
-                 x_beam_pos=b_par.x_beam_pos, y_beam_pos=b_par.y_beam_pos, save_beam_field_file_as=b_par.save_beam_field_file_as,
-                 Nx=b_par.Nx, Ny=b_par.Ny, nimag=b_par.nimag, progress_mapgen_file=(cc.progress_path+'_mapgen'))
+   
+   
+    if not skip_beam:
+        beamtim=beatim.beam_and_timing(b_par.flag_bunched_beam, b_par.fact_beam, b_par.coast_dens, b_par.beam_field_file,cc.lam_th,
+                     b_spac=b_par.b_spac, sigmaz=b_par.sigmaz,t_offs=b_par.t_offs, filling_pattern_file=b_par.filling_pattern_file, Dt=cc.Dt, t_end=cc.t_end,
+                     beam_long_prof_file=b_par.beam_long_prof_file, Dh_beam_field=b_par.Dh_beam_field, f_telescope_beam=b_par.f_telescope_beam,
+                     target_grid_beam=b_par.target_grid_beam, N_nodes_discard_beam=b_par.N_nodes_discard_beam, N_min_Dh_main_beam=b_par.N_min_Dh_main_beam,
+                     chamb=chamb,  sigmax=b_par.sigmax, sigmay=b_par.sigmay,
+                     x_beam_pos=b_par.x_beam_pos, y_beam_pos=b_par.y_beam_pos, save_beam_field_file_as=b_par.save_beam_field_file_as,
+                     Nx=b_par.Nx, Ny=b_par.Ny, nimag=b_par.nimag, progress_mapgen_file=(cc.progress_path+'_mapgen'))
+                     
+        sec_beams_list=[]
+        if flag_presence_sec_beams:
+            N_sec_beams = len(sec_b_par_list)
+            for ii in xrange(N_sec_beams):
+                print('Initialize secondary beam %d/%d' % (ii+1, N_sec_beams))
+                sb_par = sec_b_par_list[ii]
+                sec_beams_list.append(beatim.beam_and_timing(sb_par.flag_bunched_beam, sb_par.fact_beam, sb_par.coast_dens, sb_par.beam_field_file, cc.lam_th,
+                     b_spac=sb_par.b_spac, sigmaz=sb_par.sigmaz,t_offs=sb_par.t_offs, filling_pattern_file=sb_par.filling_pattern_file, Dt=cc.Dt, t_end=cc.t_end,
+                     beam_long_prof_file=sb_par.beam_long_prof_file, Dh_beam_field=sb_par.Dh_beam_field, f_telescope_beam=sb_par.f_telescope_beam,
+                     target_grid_beam=sb_par.target_grid_beam, N_nodes_discard_beam=sb_par.N_nodes_discard_beam, N_min_Dh_main_beam=sb_par.N_min_Dh_main_beam,
+                     chamb=chamb, sigmax=sb_par.sigmax, sigmay=sb_par.sigmay,
+                     x_beam_pos=sb_par.x_beam_pos, y_beam_pos=sb_par.y_beam_pos, save_beam_field_file_as=sb_par.save_beam_field_file_as,
+                     flag_secodary_beam=True, t_primary_beam=beamtim.t,
+                     Nx=sb_par.Nx, Ny=sb_par.Ny, nimag=sb_par.nimag, progress_mapgen_file=(cc.progress_path+('_mapgen_sec_%d' % ii))))
+    else:
+        beamtim = None
+        sec_beams_list=[]
+        
 
-    if cc.sparse_solver=='klu':
-        print('''sparse_solver: 'klu' no longer supported --> going to PyKLU''')
-        cc.sparse_solver='PyKLU'
 
-    spacech_ele = scc.space_charge(chamb, cc.Dh_sc, Dt_sc=cc.Dt_sc, sparse_solver=cc.sparse_solver, PyPICmode=cc.PyPICmode,
-                        f_telescope=cc.f_telescope, target_grid=cc.target_grid, N_nodes_discard=cc.N_nodes_discard, N_min_Dh_main=cc.N_min_Dh_main)
+    if not skip_spacech_ele:
+        if cc.sparse_solver=='klu':
+            print('''sparse_solver: 'klu' no longer supported --> going to PyKLU''')
+            cc.sparse_solver='PyKLU'
+        spacech_ele = scc.space_charge(chamb, cc.Dh_sc, Dt_sc=cc.Dt_sc, sparse_solver=cc.sparse_solver, PyPICmode=cc.PyPICmode,
+                            f_telescope=cc.f_telescope, target_grid=cc.target_grid, N_nodes_discard=cc.N_nodes_discard, N_min_Dh_main=cc.N_min_Dh_main)
+    else:
+        spacech_ele = None
+    
+        
+    
 
-    sec_beams_list=[]
-    if flag_presence_sec_beams:
-        N_sec_beams = len(sec_b_par_list)
-        for ii in xrange(N_sec_beams):
-            print('Initialize secondary beam %d/%d' % (ii+1, N_sec_beams))
-            sb_par = sec_b_par_list[ii]
-            sec_beams_list.append(beatim.beam_and_timing(sb_par.flag_bunched_beam, sb_par.fact_beam, sb_par.coast_dens, sb_par.beam_field_file, cc.lam_th,
-                 b_spac=sb_par.b_spac, sigmaz=sb_par.sigmaz,t_offs=sb_par.t_offs, filling_pattern_file=sb_par.filling_pattern_file, Dt=cc.Dt, t_end=cc.t_end,
-                 beam_long_prof_file=sb_par.beam_long_prof_file, Dh_beam_field=sb_par.Dh_beam_field, f_telescope_beam=sb_par.f_telescope_beam,
-                 target_grid_beam=sb_par.target_grid_beam, N_nodes_discard_beam=sb_par.N_nodes_discard_beam, N_min_Dh_main_beam=sb_par.N_min_Dh_main_beam,
-                 chamb=chamb, sigmax=sb_par.sigmax, sigmay=sb_par.sigmay,
-                 x_beam_pos=sb_par.x_beam_pos, y_beam_pos=sb_par.y_beam_pos, save_beam_field_file_as=sb_par.save_beam_field_file_as,
-                 flag_secodary_beam=True, t_primary_beam=beamtim.t,
-                 Nx=sb_par.Nx, Ny=sb_par.Ny, nimag=sb_par.nimag, progress_mapgen_file=(cc.progress_path+('_mapgen_sec_%d' % ii))))
-
-    kwargs = {}
+    kwargs_secem = {}
 
     if cc.E0 is not None:
-        kwargs.update({'E0':cc.E0})
+        kwargs_secem.update({'E0':cc.E0})
         #If E0 is not provided use default value for each object
 
     if cc.s_param is not None:
         if cc.switch_model==0 or cc.switch_model=='ECLOUD':
-            kwargs.update({'s':cc.s_param})
+            kwargs_secem.update({'s':cc.s_param})
         else:
             raise ValueError('s parameter can be changed only in the ECLOUD sec. emission model!')
 
     if cc.switch_model in (0, 'ECLOUD'):
-        kwargs['flag_costheta_delta_scale'] = cc.flag_costheta_delta_scale
-        kwargs['flag_costheta_Emax_shift'] = cc.flag_costheta_Emax_shift
-        sey_mod=SEY_model_ECLOUD(cc.Emax,cc.del_max,cc.R0,**kwargs)
+        kwargs_secem['flag_costheta_delta_scale'] = cc.flag_costheta_delta_scale
+        kwargs_secem['flag_costheta_Emax_shift'] = cc.flag_costheta_Emax_shift
+        sey_mod=SEY_model_ECLOUD(cc.Emax,cc.del_max,cc.R0,**kwargs_secem)
     elif cc.switch_model in (1, 'ACC_LOW'):
-        sey_mod=SEY_model_acc_low_ene(cc.Emax,cc.del_max,cc.R0,**kwargs)
+        sey_mod=SEY_model_acc_low_ene(cc.Emax,cc.del_max,cc.R0,**kwargs_secem)
     elif cc.switch_model=='ECLOUD_nunif':
-        sey_mod=SEY_model_ECLOUD_non_unif(chamb, cc.Emax,cc.del_max,cc.R0,**kwargs)
+        sey_mod=SEY_model_ECLOUD_non_unif(chamb, cc.Emax,cc.del_max,cc.R0,**kwargs_secem)
     elif cc.switch_model=='cos_low_ene':
-        sey_mod=SEY_model_cos_le(cc.Emax,cc.del_max,cc.R0,**kwargs)
+        sey_mod=SEY_model_cos_le(cc.Emax,cc.del_max,cc.R0,**kwargs_secem)
     elif cc.switch_model=='flat_low_ene':
         sey_mod=SEY_model_flat_le(cc.Emax,cc.del_max,cc.R0)
     elif cc.switch_model == 'from_file':
         sey_mod = SEY_model_from_file(cc.sey_file, cc.flag_factor_costheta)
-
+    elif cc.switch_model == 'perfect_absorber':
+        sey_mod = None
+    else:
+        raise ValueError('switch_model not recognized!')
 
     flag_seg = (cc.flag_hist_impact_seg==1)
 
-    impact_man=imc.impact_management(cc.switch_no_increase_energy, chamb, sey_mod, cc.E_th, cc.sigmafit, cc.mufit,
-                 cc.Dx_hist, cc.scrub_en_th, cc.Nbin_En_hist, cc.En_hist_max, thresh_low_energy=cc.thresh_low_energy,
-                 flag_seg=flag_seg, cos_angle_width=cc.cos_angle_width, secondary_angle_distribution=cc.secondary_angle_distribution)
+    if cc.switch_model=='perfect_absorber':
+        import perfect_absorber_class as pac
+        impact_man = pac.impact_management_perfect_absorber(
+            cc.switch_no_increase_energy, chamb, sey_mod, cc.E_th, cc.sigmafit,cc.mufit, cc.Dx_hist, cc.scrub_en_th,
+            cc.Nbin_En_hist, cc.En_hist_max, thresh_low_energy=cc.thresh_low_energy, flag_seg=flag_seg,
+            cos_angle_width=cc.cos_angle_width, secondary_angle_distribution=cc.secondary_angle_distribution
+        )
+    else:
+        impact_man=imc.impact_management(cc.switch_no_increase_energy, chamb, sey_mod, cc.E_th, cc.sigmafit, cc.mufit,
+                     cc.Dx_hist, cc.scrub_en_th, cc.Nbin_En_hist, cc.En_hist_max, thresh_low_energy=cc.thresh_low_energy,
+                     flag_seg=flag_seg, cos_angle_width=cc.cos_angle_width, secondary_angle_distribution=cc.secondary_angle_distribution)
 
     #resgasion_sec_beam_list=[]
     if cc.gas_ion_flag==1:
@@ -248,28 +276,31 @@ def read_input_files_and_init_components(pyecl_input_folder='./', **kwargs):
         phemiss=gpc.photoemission(cc.inv_CDF_refl_photoem_file, cc.k_pe_st, cc.refl_frac, cc.e_pe_sigma, cc.e_pe_max, cc.alimit,
                                   cc.x0_refl, cc.y0_refl, cc.out_radius, chamb, cc.phem_resc_fac, cc.energy_distribution, cc.photoelectron_angle_distribution,
                                   beamtim, cc.flag_continuous_emission)
-
     elif cc.photoem_flag in (2, 'from_file'):
         phemiss = gpc.photoemission_from_file(cc.inv_CDF_all_photoem_file, chamb, cc.phem_resc_fac, cc.energy_distribution, cc.e_pe_sigma,
                                               cc.e_pe_max, cc.k_pe_st, cc.out_radius, cc.photoelectron_angle_distribution, beamtim, cc.flag_continuous_emission)
     else:
         phemiss=None
 
-    pyeclsaver.start_observing(MP_e, beamtim, impact_man,
-                 cc.r_center, cc.Dt_En_hist, cc.logfile_path, cc.progress_path, flag_detailed_MP_info=cc.flag_detailed_MP_info,
-                 flag_movie=cc.flag_movie, flag_sc_movie=cc.flag_sc_movie, save_mp_state_time_file=cc.save_mp_state_time_file,
-                 flag_presence_sec_beams=flag_presence_sec_beams, sec_beams_list=sec_beams_list, dec_fac_secbeam_prof=cc.dec_fac_secbeam_prof,
-                 el_density_probes=cc.el_density_probes, save_simulation_state_time_file=cc.save_simulation_state_time_file,
-                 x_min_hist_det=cc.x_min_hist_det, x_max_hist_det=cc.x_max_hist_det, y_min_hist_det=cc.y_min_hist_det, y_max_hist_det=cc.y_max_hist_det,
-                 Dx_hist_det=cc.Dx_hist_det, dec_fact_out=cc.dec_fact_out, stopfile=cc.stopfile, filen_main_outp=cc.filen_main_outp,
-                 flag_cos_angle_hist=cc.flag_cos_angle_hist, cos_angle_width=cc.cos_angle_width)
+    
+    if not skip_pyeclsaver:
+        pyeclsaver.start_observing(MP_e, beamtim, impact_man,
+                     cc.r_center, cc.Dt_En_hist, cc.logfile_path, cc.progress_path, flag_detailed_MP_info=cc.flag_detailed_MP_info,
+                     flag_movie=cc.flag_movie, flag_sc_movie=cc.flag_sc_movie, save_mp_state_time_file=cc.save_mp_state_time_file,
+                     flag_presence_sec_beams=flag_presence_sec_beams, sec_beams_list=sec_beams_list, dec_fac_secbeam_prof=cc.dec_fac_secbeam_prof,
+                     el_density_probes=cc.el_density_probes, save_simulation_state_time_file=cc.save_simulation_state_time_file,
+                     x_min_hist_det=cc.x_min_hist_det, x_max_hist_det=cc.x_max_hist_det, y_min_hist_det=cc.y_min_hist_det, y_max_hist_det=cc.y_max_hist_det,
+                     Dx_hist_det=cc.Dx_hist_det, dec_fact_out=cc.dec_fact_out, stopfile=cc.stopfile, filen_main_outp=cc.filen_main_outp,
+                     flag_cos_angle_hist=cc.flag_cos_angle_hist, cos_angle_width=cc.cos_angle_width)
 
 
     if cc.track_method == 'Boris':
         dynamics=dynB.pusher_Boris(cc.Dt, cc.B0x, cc.B0y, cc.B0z,
                  cc.B_map_file, cc.fact_Bmap, cc.Bz_map_file, N_sub_steps=cc.N_sub_steps)
     elif cc.track_method == 'StrongBdip':
-        dynamics=dyndip.pusher_dipole_magnet(cc.Dt,cc.B)
+        if cc.B==-1:
+            B = 2*np.pi*b_par.beta_rel*b_par.energy_J/(c*qe*cc.bm_totlen)
+        dynamics=dyndip.pusher_dipole_magnet(cc.Dt, B)
     elif cc.track_method == 'StrongBgen':
         dynamics=dyngen.pusher_strong_B_generalized(cc.Dt, cc.B0x, cc.B0y,
                  cc.B_map_file, cc.fact_Bmap, cc.B_zero_thrhld)
