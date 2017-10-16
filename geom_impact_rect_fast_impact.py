53ee7706024315a0475127cfe311cdda131e6909
giadarol <giovanni.iadarola@cern.ch>
2017-10-16 17:01:58 +0200

    Changed vers numb

--- geom_impact_rect_fast_impact.py
+++ geom_impact_rect_fast_impact.py
@@ -7,7 +7,7 @@
 #
 #     This file is part of the code:
 #
-#                   PyECLOUD Version 6.5.0
+#                   PyECLOUD Version 6.5.1
 #
 #
 #     Author and contact:   Giovanni IADAROLA
