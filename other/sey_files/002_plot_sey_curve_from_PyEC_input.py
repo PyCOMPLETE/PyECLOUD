b09e10a57ead9677ce0337cbc9689903765be151
giadarol <giovanni.iadarola@cern.ch>
2018-02-16 12:24:16 +0100

    Extraction of sey_curves included in impact man

--- other/sey_files/002_plot_sey_curve_from_PyEC_input.py
+++ other/sey_files/002_plot_sey_curve_from_PyEC_input.py
@@ -18,27 +18,12 @@ beamtim,MP_e, dynamics,impact_man, pyeclsaver, \
             pyecl_input_folder=pyecl_input_folder, skip_beam=True,
             skip_pyeclsaver=True, skip_spacech_ele=True)
             
-sey_mod = impact_man.sey_mod
-
-E_impact_eV = np.array(list(np.arange(0, 499., 1.)) + list(np.arange(500., 2000, 5)))
-cos_theta = np.linspace(0, 1., 10)
-nel_impact = 1. + 0.*E_impact_eV
 
 n_rep = 1000
+E_impact_eV_test = np.array(list(np.arange(0, 499., 1.)) + list(np.arange(500., 2000, 5)))
+cos_theta_test = np.linspace(0, 1., 10)
 
-del_true_mat = np.zeros((len(cos_theta), len(E_impact_eV)))
-del_elast_mat = np.zeros((len(cos_theta), len(E_impact_eV)))
-
-for i_ct, ct in enumerate(cos_theta):
-    print('%d/%d'%(i_ct+1, len(cos_theta))) 
-    for i_ene, Ene in enumerate(E_impact_eV):
-        
-        nel_emit, flag_elast, flag_truesec = sey_mod.SEY_process(nel_impact=np.ones(n_rep), 
-                        E_impact_eV=Ene*np.ones(n_rep), costheta_impact=np.ones(n_rep)*ct, i_impact=np.array(n_rep*[0]))
-                        
-        del_true_mat[i_ct, i_ene] = np.mean(nel_emit)*float(np.sum(flag_truesec))/float(n_rep)
-        del_elast_mat[i_ct, i_ene] = np.mean(nel_emit)*float(np.sum(flag_elast))/float(n_rep)
-        
+del_true_mat, del_elast_mat = impact_man.extract_sey_curves(n_rep, E_impact_eV_test, cos_theta_test)
 
 pl.close('all')
 ms.mystyle_arial()
@@ -47,11 +32,11 @@ fig1 = pl.figure(1, figsize=(2*8,6))
 fig1.set_facecolor('w')
 sp1 = fig1.add_subplot(1,2,1)
 sp2 = fig1.add_subplot(1,2,2, sharex=sp1)
-for i_ct, ct in enumerate(cos_theta):
-    thiscol = ms.colorprog(i_ct, len(cos_theta))
+for i_ct, ct in enumerate(cos_theta_test):
+    thiscol = ms.colorprog(i_ct, len(cos_theta_test))
     label = 'costheta=%.2f'%ct
-    sp1.plot(E_impact_eV, del_true_mat[i_ct, :], color=thiscol, label=label)
-    sp2.plot(E_impact_eV, del_elast_mat[i_ct, :], color=thiscol, label=label)
+    sp1.plot(E_impact_eV_test, del_true_mat[i_ct, :], color=thiscol, label=label)
+    sp2.plot(E_impact_eV_test, del_elast_mat[i_ct, :], color=thiscol, label=label)
 
 sp2.legend(loc='best', prop={'size':14})  
 sp1.set_ylabel('Delta true')
