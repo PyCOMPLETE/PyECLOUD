Fix rho calculation: np.sum --> sum, np.sum sums the full array to one number.

buildup_simulation.py
@@ -155,7 +155,7 @@ class BuildupSimulation(object):
                     spacech_ele.recompute_spchg_efield(MP_e, t_curr=beamtim.tt_curr, flag_solve=flag_solve, flag_reset=flag_reset)
 
                     # Copy rho to cloud
-                    cloud.rho = spacech_ele.rho - np.sum([cl.rho for cl in cloud_list[:i_cloud]])
+                    cloud.rho = spacech_ele.rho - sum([cl.rho for cl in cloud_list[:i_cloud]])
 
 
             for cloud in cloud_list: