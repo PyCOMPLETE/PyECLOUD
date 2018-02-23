dc73b0cae4672f69dd1c18e04c5652e3174b43c2
lmether <lotta.mether@cern.ch>
2018-02-23 10:45:07 +0100

    New naming conventions implemented in parse cloud file: default cloud can be given a name in input files, cloud output name is determined by cloud file name.

--- parse_cloud_file.py
+++ parse_cloud_file.py
@@ -8,7 +8,15 @@ class cloud_descr_from_file:
         if cloudfilename is None:
             print('Copying cloud parameters from main input')
 
-            cloudfilename = 'default'
+            if default_param_obj.cloud_name is not None:
+                cloud_name = default_param_obj.cloud_name
+                cloud_output_name = default_param_obj.filen_main_outp.split('.mat')[0]
+                cloud_output_name += '_' + cloud_name + '.mat'
+            else:
+                cloud_name = 'default'
+                cloud_output_name = default_param_obj.filen_main_outp
+
+
             config_dict = {}
             inp_spec.copy_to_config_dict(config_dict, 'additional_cloud_parameters', default_param_obj)
 
@@ -20,12 +28,19 @@ class cloud_descr_from_file:
         
             # Verify validity of provided module w.r.t. parameters_dict
             inp_spec.assert_module_has_parameters(cloud_params, 'additional_cloud_parameters')
-        
+
             # Create config_dict with cloud parameters (unspecified parameters are set to values in default object)
             config_dict = {}
             inp_spec.update_config_dict(config_dict, cloud_params, 'additional_cloud_parameters', default_obj=default_param_obj)
 
-        config_dict['cloudname'] = cloudfilename 
+            # Make names for cloud and cloud output
+            cloud_name = cloudfilename.split('/')[-1].split('.cloud')[0]
+
+            cloud_output_name = default_param_obj.filen_main_outp.split('.mat')[0]
+            cloud_output_name += '_' + cloud_name + '.mat'
+
+        config_dict['filen_main_outp'] = cloud_output_name
+        config_dict['cloud_name'] = cloud_name
 
         cc = mlm.obj_from_dict(config_dict)
 
