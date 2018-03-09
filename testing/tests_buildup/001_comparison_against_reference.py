23d6363d0af3a566b06cf7b462d85ee9b0ef1ba5
lmether <lotta.mether@cern.ch>
2018-03-09 09:44:22 +0100

    Put selected folder in test scripts back to default.

--- testing/tests_buildup/001_comparison_against_reference.py
+++ testing/tests_buildup/001_comparison_against_reference.py
@@ -14,7 +14,7 @@ import PyECLOUD.mystyle as ms
 pl.close('all')
 
 sim_folder = 'LHC_ArcDipReal_450GeV_sey1.70_2.5e11ppb_bl_1.00ns'
-sim_folder = 'LHC_ArcDipReal_450GeV_sey1.00_2.5e11ppb_bl_1.00ns_gas_ionization'
+#sim_folder = 'LHC_ArcDipReal_450GeV_sey1.00_2.5e11ppb_bl_1.00ns_gas_ionization'
 #sim_folder = 'LHC_ArcDipReal_450GeV_sey1.70_2.5e11ppb_bl_1.00ns_change_s_and_E0'
 #sim_folder = 'LHC_ArcDipReal_450GeV_sey1.70_2.5e11ppb_bl_1.00ns_multigrid'
 #sim_folder = 'LHC_ArcQuadReal_450GeV_sey1.65_2.5e11ppb_bl_1.00ns'
