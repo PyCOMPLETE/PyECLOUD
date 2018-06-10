9f33017375ca8c06cec70e445a4e1e4157bc55d6
giadarol <giovanni.iadarola@cern.ch>
2018-06-10 10:26:47 +0200

    Gas ion test is now with non-uniform time steps

--- testing/tests_buildup/004_compare_unif_vs_non_unif.py
+++ testing/tests_buildup/004_compare_unif_vs_non_unif.py
@@ -11,8 +11,11 @@ import matplotlib.pyplot as plt
 # obu = mlm.myloadmat_to_obj('LHC_ArcDipReal_450GeV_sey1.70_2.5e11ppb_bl_1.00ns/Pyecltest_angle3D_ref.mat')
 # obn = mlm.myloadmat_to_obj('LHC_ArcDipReal_450GeV_sey1.70_2.5e11ppb_bl_1.00ns_nonuniftime/Pyecltest_angle3D.mat')
 
-obu = mlm.myloadmat_to_obj('LHC_ArcDipReal_6500GeV_sey_1.70_1.1e11ppb_b1_1.00ns/Pyecltest_angle3D_ref.mat')
-obn = mlm.myloadmat_to_obj('LHC_ArcDipReal_6500GeV_sey_1.70_1.1e11ppb_b1_1.00ns/Pyecltest_angle3D.mat')
+obu = mlm.myloadmat_to_obj('LHC_ArcDipReal_450GeV_sey1.00_2.5e11ppb_bl_1.00ns_gas_ionization/Pyecltest_angle3D_ref.mat')
+obn = mlm.myloadmat_to_obj('LHC_ArcDipReal_450GeV_sey1.00_2.5e11ppb_bl_1.00ns_gas_ionization/Pyecltest_angle3D.mat')
+
+# obu = mlm.myloadmat_to_obj('LHC_ArcDipReal_6500GeV_sey_1.70_1.1e11ppb_b1_1.00ns/Pyecltest_angle3D_ref.mat')
+# obn = mlm.myloadmat_to_obj('LHC_ArcDipReal_6500GeV_sey_1.70_1.1e11ppb_b1_1.00ns/Pyecltest_angle3D.mat')
 
 
 plt.close('all')
