1d07fadbd3f0764e46b99a6725ddb252c61bd59e
giadarol <giovanni.iadarola@cern.ch>
2018-10-05 18:17:12 +0200

    Multibunch distorition example

---
+++ other/response_to_beam_distorsion/multibunch/001_plot_result.py
@@ -0,0 +1,23 @@
+import sys, os
+BIN = os.path.expanduser("../../../../")
+sys.path.append(BIN)
+
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+#from colorsys import hsv_to_rgb
+import os
+import PyECLOUD.myloadmat_to_obj as mlm
+import matplotlib.gridspec as gridspec
+import PyECLOUD.mystyle as ms
+
+plt.close('all')
+
+
+ms.mystyle_arial(fontsz=14)
+
+ob = mlm.myloadmat_to_obj('./test_saving__iter0.mat')   # load dictionary of the current simulation
+
+plt.figure(1)
+plt.pcolormesh(ob.nel_hist, shading='Gouraud')
+plt.show()
