5c9f6d52a960d8584d44edd60ef908f735ff4d74
giadarol <giovanni.iadarola@cern.ch>
2018-11-12 16:45:31 +0100

    Some more timing of tests

--- testing/tests_buildup/000_run_simulation.py
+++ testing/tests_buildup/000_run_simulation.py
@@ -52,12 +52,16 @@ filen_main_outp = sim_folder + '/Pyecltest_angle%s.mat' % args.angle_dist_func
 time_0 = time.time()
 sim = BuildupSimulation(pyecl_input_folder=sim_folder, filen_main_outp=filen_main_outp,
                         secondary_angle_distribution=angle_distribution, photoelectron_angle_distribution=angle_distribution)
+time_1 = time.time()
 sim.run()
-time_needed = time.time() - time_0
+time_2 = time.time()
+
+time_init = time_1 - time_0
+time_run = time_2 - time_1
 
 
 print('')
-print('Test simulation done in %.2f s!' % time_needed)
+print('Test simulation done in %.2f s (init: %.1f s, run: %.1f s)!' % (time_init + time_run, time_init, time_run))
 print('To inspect the results you can run:')
 print('001_comparison_against_reference.py')
 print('')
