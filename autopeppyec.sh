0e82d8c334a8de60520b408a6a2f929ec1386d19
giadarol <giovanni.iadarola@cern.ch>
2018-11-13 23:24:09 +0100

    Added a comment

--- autopeppyec.sh
+++ autopeppyec.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 autopep8 --in-place --recursive --select W291,W293,E303,E225,E226,E221,E222,E231,E251,E703,E127,E128,E122,E271,E241 . 
+
+# In the editor I am ignoring: E501, E502, E265, E228,E266,E261
