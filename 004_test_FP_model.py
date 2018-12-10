Fixed calculation of delta_ts; Changed tests

004_test_FP_model.py
@@ -21,10 +21,10 @@ round_to_digits = 4
 
 plt.close('all')
 # Subplots
-fig, ((ax1, ax2, ax3, ax4), (ax5, ax6, ax7, ax8)) = plt.subplots(2, 4, sharex=True, figsize=(1.8 * 12, 12), facecolor='w')
-plt.suptitle('Furman-Pivi tests', fontsize=25)
+fig, ((ax1, ax2, ax4), (ax5, ax6, ax8)) = plt.subplots(2, 3, sharex=True, figsize=(1.8 * 12, 12), facecolor='w')
+plt.suptitle('Furman-Pivi tests: Energy distributions', fontsize=30)
 
-axlist = [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8]
+axlist = [ax1, ax2, ax4, ax5, ax6, ax8]
 for ax in axlist:
     ax.grid(alpha=alpha)
     ax.set_xlabel('Energy [eV]')
@@ -89,10 +89,11 @@ ax8.legend()
 fig.subplots_adjust(left=0.05, right=0.95)
 
 # SEY components
-sp1 = plt.subplot(1, 4, 1)
-sp2 = plt.subplot(1, 4, 2, sharex=sp1, sharey=sp1)
-sp3 = plt.subplot(1, 4, 3, sharex=sp1, sharey=sp1)
-sp4 = plt.subplot(1, 4, 4, sharex=sp1, sharey=sp1)
+plt.figure(2, figsize=(16, 12), facecolor='w')
+sp1 = plt.subplot(2, 2, 1)
+sp2 = plt.subplot(2, 2, 2, sharex=sp1, sharey=sp1)
+sp3 = plt.subplot(2, 2, 3, sharex=sp1, sharey=sp1)
+sp4 = plt.subplot(2, 2, 4, sharex=sp1, sharey=sp1)
 
 energy = np.linspace(0., 2000, num=int(1e3))
 sp1.plot(energy, test_obj._delta_e(energy, 1), label=r'$\delta_e$', color='r', linewidth=linewid)
@@ -109,13 +110,13 @@ for costheta in np.linspace(0, 1, 10):
 
 for sp in [sp1, sp2, sp3, sp4]:
     sp.grid(alpha=alpha)
-plt.title('SEY components', fontsize=25)
+plt.suptitle('Furman Pivi test: SEY components', fontsize=30)
 
 # Tests for multiple nn
 energy = np.linspace(0.001, E_0_single, num=int(1e5))
 E_0 = np.array([E_0_single] * int(1e5))
-fig, axarr = plt.subplots(2, 10, sharex=True, figsize=(1.8 * 12, 12), facecolor='w')
-plt.suptitle('Furman-Pivi tests', fontsize=25)
+fig2, axarr = plt.subplots(2, 10, sharex=True, figsize=(1.8 * 12, 12), facecolor='w')
+plt.suptitle('Furman-Pivi tests: True secondary energy distributions', fontsize=30)
 for kk in np.arange(1, 10.1, 1):
     nn = np.repeat(kk, 1e5)
     prob_density_ts, _ = test_obj.true_sec_energy_PDF(delta_ts=delta_ts[0], nn=kk, E_0=E_0[0], energy=energy)

005_test_FP_model_sey_curves.py
@@ -77,7 +77,7 @@ def extract_sey_curves(n_rep, E_impact_eV_test, cos_theta_test, charge, mass):
 
 
 cos_theta_test = np.linspace(0, 1., 10)
-E_impact_eV_test = np.array(list(np.arange(0, 499., 5.)) + list(np.arange(500., 2000, 25)))
+E_impact_eV_test = np.array(list(np.arange(0, 499., 1.)) + list(np.arange(500., 2000, 5.)))
 n_rep = 10000
 
 deltas = extract_sey_curves(n_rep, E_impact_eV_test, cos_theta_test, charge=qe, mass=me)
@@ -89,13 +89,14 @@ del_absorb_mat = deltas['absorb']
 plt.close('all')
 ms.mystyle_arial()
 
-fig1 = plt.figure(1, figsize=(4 * 8, 8))
+fig1 = plt.figure(1, figsize=(3 * 8, 2 * 8))
 fig1.set_facecolor('w')
-sp1 = fig1.add_subplot(1, 5, 1)
-sp2 = fig1.add_subplot(1, 5, 2, sharex=sp1)
-sp3 = fig1.add_subplot(1, 5, 3, sharex=sp1)
-sp4 = fig1.add_subplot(1, 5, 4, sharex=sp1)
-sp5 = fig1.add_subplot(1, 5, 5, sharex=sp1)
+sp1 = fig1.add_subplot(2, 3, 1)
+sp2 = fig1.add_subplot(2, 3, 2, sharex=sp1)
+sp3 = fig1.add_subplot(2, 3, 3, sharex=sp1)
+sp4 = fig1.add_subplot(2, 3, 4, sharex=sp1)
+sp5 = fig1.add_subplot(2, 3, 5, sharex=sp1)
+sp6 = fig1.add_subplot(2, 3, 6, sharex=sp1)
 
 for i_ct, ct in enumerate(cos_theta_test):
     thiscol = ms.colorprog(i_ct, len(cos_theta_test))
@@ -105,18 +106,39 @@ for i_ct, ct in enumerate(cos_theta_test):
     sp3.plot(E_impact_eV_test, del_rediff_mat[i_ct, :], color=thiscol, label=label)
     sp4.plot(E_impact_eV_test, del_absorb_mat[i_ct, :], color=thiscol, label=label)
     sp5.plot(E_impact_eV_test, del_true_mat[i_ct, :] + del_rediff_mat[i_ct, :] + del_elast_mat[i_ct, :], color=thiscol, label=label)
+    sp6.plot(E_impact_eV_test, del_true_mat[i_ct, :] + del_elast_mat[i_ct, :], color=thiscol, label=label)
 
+sp3.plot(0, 0, 'white', label='Model')
 sp3.legend(loc='best', prop={'size': 14})
 sp1.set_ylabel('Delta true')
 sp2.set_ylabel('Delta elast')
 sp3.set_ylabel('Delta rediff')
 sp4.set_ylabel('Delta absorb')
 sp5.set_ylabel('Delta total')
+sp6.set_ylabel(r'$\delta_{ts} + \delta_{e}$')
 
-for sp in [sp1, sp2, sp3, sp4, sp5]:
+for sp in [sp1, sp2, sp3, sp4, sp5, sp6]:
     sp.grid('on')
     sp.set_xlabel('Electron energy [eV]')
 
 plt.subplots_adjust(right=0.99, left=.05)
+
+
+test_obj = fp.SEY_model_FP_Cu()  # 276.8, 1.8848)
+
+energy = np.linspace(0., 2000, num=int(1e3))
+
+for costheta in np.linspace(0, 1, 10):
+    delta_ts_vec = test_obj._delta_ts(energy, costheta)
+    delta_e_vec = test_obj._delta_e(energy, costheta)
+    delta_r_vec = test_obj._delta_r(energy, costheta)
+
+    sp2.plot(energy, delta_e_vec, color='k', linewidth=linewid)
+    sp3.plot(energy, delta_r_vec, color='k', linewidth=linewid)
+    sp1.plot(energy, delta_ts_vec, color='k', linewidth=linewid)
+    sp5.plot(energy, delta_r_vec + delta_ts_vec + delta_e_vec, color='k', linewidth=linewid)
+    sp6.plot(energy, delta_ts_vec + delta_e_vec, color='k', linewidth=linewid)
+
+plt.suptitle('SEY extraction tests: Furman-Pivi model', fontsize=30)
 
 plt.show()

sec_emission_model_furman_pivi.py
@@ -377,7 +377,9 @@ class SEY_model_furman_pivi():
         N_true_sec = np.sum(flag_truesec)
         n_add_total = 0
         if N_true_sec > 0:
-            delta_ts = self._delta_ts(E_impact_eV[flag_truesec], costheta_impact[flag_truesec])
+            # delta_ts = self._delta_ts(E_impact_eV[flag_truesec], costheta_impact[flag_truesec])
+            delta_e, delta_r, delta_ts = self._yield_fun_furman_pivi(E_impact_eV[flag_truesec], costheta_impact[flag_truesec])
+            delta_ts = delta_ts / (1 - delta_e - delta_r)  # delta_ts^prime in FP paper, eq. (39)
             n_add = np.zeros_like(flag_truesec, dtype=int)
             n_add[flag_truesec] = random.poisson(lam=delta_ts)
             n_add_flag_true_sec = n_add[flag_truesec]