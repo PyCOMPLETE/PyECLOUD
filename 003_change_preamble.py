4d7e1e4f1246b4ce904871cea36e20f1ac394beb
giadarol <giovanni.iadarola@cern.ch>
2019-02-11 18:52:00 +0100

    Change version number to 7.6.1

--- 003_change_preamble.py
+++ 003_change_preamble.py
@@ -20,7 +20,7 @@ preamble_new = """#-Begin-preamble----------------------------------------------
 #
 #     This file is part of the code:
 #
-#                   PyECLOUD Version 7.6.0
+#                   PyECLOUD Version 7.6.1
 #
 #
 #     Main author:          Giovanni IADAROLA
