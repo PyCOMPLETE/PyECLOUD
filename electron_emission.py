efd0718acbe108eb211e7fc16e20f0d492778a9a
Eric Wulff <eric.g.t.wulff@gmail.com>
2019-02-20 15:56:23 +0100

    Handling low energy electrons separately, now running fine.

--- electron_emission.py
+++ electron_emission.py
@@ -72,18 +72,19 @@ def sec_energy_hilleret_model2(switch_no_increase_energy, Ngen, sigmafit, mufit,
             flag_above_th = (en_eV > E_th)
             Nabove_th = np.sum(flag_above_th)
 
-    elif switch_no_increase_energy == 2:
-        #En_impact_eV[En_impact_eV < 1.] = 0.
+    elif switch_no_increase_energy == 2:  # Cut emitted energy at En_impact_eV
+        flag_low_energy = En_impact_eV < thresh_low_energy
+
         en_eV = random.lognormal(mufit, sigmafit, Ngen)
-        flag_above_th = ((en_eV > En_impact_eV) & (En_impact_eV != 0))
+        flag_above_th = ((en_eV > En_impact_eV) & (~flag_low_energy))
         Nabove_th = np.sum(flag_above_th)
 
         while Nabove_th > 0:
             en_eV[flag_above_th] = random.lognormal(mufit, sigmafit, Nabove_th)
 
-            flag_above_th = ((en_eV > En_impact_eV) & (En_impact_eV != 0))
+            flag_above_th = ((en_eV > En_impact_eV) & (~flag_low_energy))
             Nabove_th = np.sum(flag_above_th)
-        en_eV[En_impact_eV == 0] = 0.
+        en_eV[flag_low_energy] = random.rand() * En_impact_eV[flag_low_energy]
 
     elif switch_no_increase_energy == 1:
 
