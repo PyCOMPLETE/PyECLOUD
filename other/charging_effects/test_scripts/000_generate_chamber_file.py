a21a6c4ee1ed5b4c320edb06319fd1277d99a469
giadarol <giovanni.iadarola@cern.ch>
2019-03-02 16:53:37 +0100

    Update test scritps

--- other/charging_effects/test_scripts/000_generate_chamber_file.py
+++ other/charging_effects/test_scripts/000_generate_chamber_file.py
@@ -35,14 +35,15 @@ sio.savemat('chamber.mat',{\
 'x_aper':np.max(Vx_unif),
 'y_aper':np.max(Vy_unif),
 
-'del_max_segments': Vx_unif*0. + 1.5,
+'del_max_segments': Vx_unif*0. + 1.9,
 'R0_segments': Vx_unif*0. + 0.7,
 'Emax_segments': Vx_unif*0. + 300.,
 
 'flag_charging':  Vx_unif*0. + 1,
 'Q_max_segments': Vx_unif*0. + 1e-12*1e6, #1e-12 C/mm^2
-'EQ_segments': Vx_unif*0. + 10.,
-
+'EQ_segments': Vx_unif*0. + 20.,
+'tau_segments': Vx_unif*0. ,
+ 
 }, oned_as='row')
         
 pl.close('all')   
--- other/charging_effects/test_scripts/001_some_checks.py
+++ other/charging_effects/test_scripts/001_some_checks.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 from PyECLOUD import geom_impact_poly_fast_impact as gipfi
 import PyECLOUD.sec_emission_model_ECLOUD_nunif as se
+import PyECLOUD.mystyle as ms
 
 chamb = gipfi.polyg_cham_geom_object('chamber.mat', flag_non_unif_sey=True)
 
@@ -13,23 +14,41 @@ semod = se.SEY_model_ECLOUD_non_unif_charging(chamb,
         thresh_low_energy=-1, 
         secondary_angle_distribution='cosine_3D')
 
-Q_Qmax_test_vect = np.linspace(0, 1.1, 5)
+Q_Qmax_test_vect = np.linspace(0, 1, 6)
 
-E_test = np.linspace(0, 5000, 10000)
+E_test = np.linspace(0, 1600, 10000)
 
 plt.close('all')
+ms.mystyle_arial(fontsz=16)
 
-for Q_Qmax in Q_Qmax_test_vect:
+fig1 = plt.figure(1)
+fig1.set_facecolor('w')
+ax1 = fig1.add_subplot(1,1,1)
+
+for iq, Q_Qmax in enumerate(Q_Qmax_test_vect):
+
+    colorcurr = ms.colorprog(iq, len(Q_Qmax_test_vect), cm='rainbow')
    
     semod.Q_segments[:] = Q_Qmax*semod.Q_max_segments
     
-    yiel, flag_elast, flag_truesec = semod.SEY_process(nel_impact=E_test*0+1.,
+    yiel, flag_elast, flag_truesec = semod.SEY_process(
+            nel_impact=E_test*0+1.,
             E_impact_eV=E_test, 
             costheta_impact=E_test*0+1.,
             i_impact=np.int_(E_test*0+1))
     
-    plt.plot(E_test, yiel)
+    ax1.plot(E_test, yiel, label='%.2f'%Q_Qmax, linewidth=2,
+                color=colorcurr)
 
+ax1.grid(True)
+ax1.set_xlabel('Energy [eV]')
+ax1.set_ylabel('Secondary Electron Yield')
+ax1.legend(loc='lower right', prop={'size':16}, title='Q/Qmax',
+        ncol=2)
+
+fig1.subplots_adjust(
+        bottom=.14
+        )
 # Check charge accumulation
 semod.Q_segments[:] = 0.
 
