33004061aedbee6fc87c91d86d34235b00539bad
giadarol <giovanni.iadarola@cern.ch>
2019-03-08 15:37:33 +0100

    Change version number to 7.7.0

--- pyecloud_saver.py
+++ pyecloud_saver.py
@@ -7,7 +7,7 @@
 #
 #     This file is part of the code:
 #
-#                   PyECLOUD Version 7.6.1
+#                   PyECLOUD Version 7.7.0
 #
 #
 #     Main author:          Giovanni IADAROLA
@@ -94,7 +94,7 @@ class pyecloud_saver:
         print(git_branch)
 
         with open(self.logfile_path, 'w') as flog:
-            flog.write('PyECLOUD Version 7.6.1\n')
+            flog.write('PyECLOUD Version 7.7.0\n')
             flog.write('%s\n' % git_hash)
             flog.write('%s\n' % git_branch)
             flog.write('Simulation started on %s\n' % timestr)
