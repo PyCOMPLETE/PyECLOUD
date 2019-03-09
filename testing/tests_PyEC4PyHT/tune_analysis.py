0c9836d55429f053f2b28d1cd3c60f2777c9a84c
giadarol <giovanni.iadarola@cern.ch>
2019-03-10 00:14:11 +0100

    PySUSSIX to NAFFlib

--- testing/tests_PyEC4PyHT/tune_analysis.py
+++ testing/tests_PyEC4PyHT/tune_analysis.py
@@ -1,56 +1,30 @@
 import numpy as np
-from PySUSSIX import Sussix
+import NAFFlib
 import sys
 
 
 def tune_analysis(x_i, xp_i, y_i, yp_i):
-		n_turns = x_i.shape[1]
-		macroparticlenumber = x_i.shape[0]
+    n_turns = x_i.shape[1]
+    macroparticlenumber = x_i.shape[0]
 
-		qx_i = np.empty(macroparticlenumber)
-		qy_i = np.empty(macroparticlenumber)
+    qx_i = np.empty(macroparticlenumber)
+    qy_i = np.empty(macroparticlenumber)
 
-		spectrum_x = np.abs(np.fft.fft(x_i, axis=1))[:, :n_turns / 2]
-		tune_peak_x = np.float_(np.argmax(spectrum_x, axis=1)) / float(n_turns)
+    print 'analysing particle spectra ... this may take some time.'
+    for p_idx in xrange(macroparticlenumber):
+        
+        qx_i[p_idx] = NAFFlib.get_tune(x_i[p_idx, :])
+        qy_i[p_idx] = NAFFlib.get_tune(y_i[p_idx, :])
 
-		spectrum_y = np.abs(np.fft.fft(y_i, axis=1))[:, :n_turns / 2]
-		tune_peak_y = np.float_(np.argmax(spectrum_y, axis=1)) / float(n_turns)
+        sys.stdout.write('\rparticle %d'%p_idx)
 
-		print 'analysing particle spectra ... this may take some time.'
-		for p_idx in xrange(macroparticlenumber):
+    x_centroid = np.mean(x_i, axis=0)
+    y_centroid = np.mean(y_i, axis=0)
 
-			SX = Sussix()
-			SX.sussix_inp(nt1=1, nt2=n_turns, idam=2, ir=0, tunex=tune_peak_x[p_idx], tuney=tune_peak_y[p_idx])
+    #print x_centroid.shape
+    qx_centroid = NAFFlib.get_tune(x_centroid) 
+    qy_centroid = NAFFlib.get_tune(y_centroid) 
 
-			SX.sussix(x_i[p_idx, :], xp_i[p_idx, :],
-                            y_i[p_idx, :], yp_i[p_idx, :],
-                            x_i[p_idx, :], xp_i[p_idx, :])
-			qx_i[p_idx] = SX.ox[0]
-			qy_i[p_idx] = SX.oy[0]
-
-			sys.stdout.write('\rparticle %d'%p_idx)
-
-		x_centroid = np.mean(x_i, axis=0)
-		xp_centroid = np.mean(xp_i, axis=0)
-		y_centroid = np.mean(y_i, axis=0)
-		yp_centroid = np.mean(yp_i, axis=0)
-
-		#print x_centroid.shape
-
-		spectrum_x_centroid = np.abs(np.fft.fft(x_centroid))[:n_turns / 2]
-		tune_peak_x_centroid = np.float_(np.argmax(spectrum_x_centroid)) / float(n_turns)
-
-		spectrumy_centroid = np.abs(np.fft.fft(x_centroid))[:n_turns / 2]
-		tune_peak_y_centroid = np.float_(np.argmax(spectrumy_centroid)) / float(n_turns)
-
-		SX = Sussix()
-		SX.sussix_inp(nt1=1, nt2=n_turns, idam=2, ir=0, tunex=tune_peak_x_centroid, tuney=tune_peak_y_centroid)
-		SX.sussix(x_centroid, xp_centroid,
-                    y_centroid, yp_centroid,
-                    x_centroid, xp_centroid)
-		qx_centroid = SX.ox[0]
-		qy_centroid = SX.oy[0]
-
-		return qx_i, qy_i, qx_centroid, qy_centroid
+    return qx_i, qy_i, qx_centroid, qy_centroid
 
 
