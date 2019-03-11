132d5ab590a817aac96df89e39d6a91ab0b6737f
Eric Wulff <eric.g.t.wulff@gmail.com>
2019-03-11 17:15:04 +0100

    Working on fitting FP model to ECLOUD

--- testing/tests_modules/010_fit_FP_parameters.py
+++ testing/tests_modules/010_fit_FP_parameters.py
@@ -9,6 +9,7 @@ from scipy.special import binom
 from scipy.misc import factorial
 from scipy.integrate import simps
 import sec_emission_model_furman_pivi as fp
+import scipy
 
 
 plt.close('all')
@@ -18,7 +19,7 @@ fontsz = 20
 alph = 0.5
 
 
-# Elastic component
+# Elastic
 def del_elas_ECLOUD(energy, R_0=0.7, E_max=332., E_0=150.):
     del_elas = R_0 * ((np.sqrt(energy) - np.sqrt(energy + E_0)) / (np.sqrt(energy) + np.sqrt(energy + E_0)))**2
     return del_elas
@@ -29,6 +30,10 @@ def del_elas_FP(energy, p1EInf, p1Ehat, ww, pp, p0=[1., 1., 1., 1.]):
     exp_factor = -(np.abs(energy - eEHat) / ww)**pp / pp
     delta_e0 = p1EInf + (p1Ehat - p1EInf) * np.exp(exp_factor)
     return delta_e0
+
+
+def pdf_elas_FP(energy, E_imp, sigma_e=2.):
+    return 2 * np.exp(-(energy - E_imp)**2 / (2 * sigma_e**2)) / (np.sqrt(2 * np.pi) * sigma_e * scipy.special.erf(E_imp / np.sqrt(2) / sigma_e))
 
 
 energy = np.linspace(0., 800, num=int(1e3))
@@ -50,7 +55,7 @@ sp1.grid(alpha=alph)
 sp1.text(310, 0.3, 'Fitted parameters: \np1EInf: %f \np1EHat: %f \nW: %f \np: %f' % (popt[0], popt[1], popt[2], popt[3]))
 
 
-# True secondary component
+# True secondary
 def del_true_ECLOUD(energy, cos_theta, del_max, s=1.35, E_max=332.):
     E_max_tilde = E_max * (1. + 0.7 * (1. - cos_theta))
     x = energy / E_max_tilde
@@ -102,54 +107,6 @@ sp2.text(310, 0.3, 'Fitted parameters: \nt1: %f \nt2: %f' % (popt_true[0], popt_
 fig.subplots_adjust(left=0.06, right=0.95)
 
 
-# Energy distributions
-M_cut = 10
-choice = 'poisson'
-
-
-def true_sec_energy_PDF(delta_ts, nn, E_0, energy):
-    """Gives the PDF for true secondary electrons."""
-    if nn == 0:
-        raise ValueError('nn = 0, you cannot emit zero electrons.')
-    p_n = np.array([2.5, 3.3, 2.5, 2.5, 2.8, 1.3, 1.5, 1.5, 1.5, 1.5])
-    eps_n = np.array([1.5, 1.75, 1., 3.75, 8.5, 11.5, 2.5, 3., 2.5, 3.])
-
-    nn_all = np.arange(0, M_cut + 1, 1)
-
-    if choice == 'poisson':
-        P_n_ts = np.squeeze(delta_ts**nn_all / factorial(nn_all) * np.exp(-delta_ts))
-    elif choice == 'binomial':
-        p = delta_ts / M_cut
-        P_n_ts = np.squeeze(binom(M_cut, nn) * (p)**nn_all * (1 - p)**(M_cut - nn_all))
-    else:
-        raise ValueError('choice must be either \'poisson\' or \'binomial\'')
-
-    P_n_ts = P_n_ts / np.sum(P_n_ts)
-    P_n_ts_return = P_n_ts[int(nn)]
-    eps_curr = eps_n[int(nn - 1)]
-    p_n_curr = p_n[int(nn - 1)]
-
-    if E_0 == 0:
-        F_n = 0
-    else:
-        F_n = 1
-    f_n_ts = F_n * energy**(p_n_curr - 1) * np.exp(-energy / eps_curr)
-    area = simps(f_n_ts, energy)
-    f_n_ts = f_n_ts / area  # normalisation
-
-    return f_n_ts, P_n_ts_return
-
-
-def average_true_sec_energy_PDF(delta_ts, E_0, energy):
-    nns = np.arange(1, self.M_cut + 1, 1)
-    average_f_n_ts = np.zeros_like(energy)
-    for ii in nns:
-        f_n_ts, P_n_ts = self.true_sec_energy_PDF(delta_ts=delta_ts, nn=ii, E_0=E_0, energy=energy)
-        average_f_n_ts = average_f_n_ts + f_n_ts * P_n_ts * ii
-    area = simps(average_f_n_ts, energy)
-    return average_f_n_ts / area
-
-
 def hilleret_energy(energy):
     sigmafit = 1.0828
     mufit = 1.6636
@@ -160,6 +117,7 @@ def simple_pdf(energy, pp, eps):
     pdf = energy**(pp - 1) * np.exp(-energy / eps)
     area = simps(pdf, energy)
     return pdf / area
+
 
 energy = np.linspace(0.0000001, 35., num=int(1e3))
 ene_hilleret = hilleret_energy(energy)
@@ -177,41 +135,57 @@ sp3.grid(alpha=alph)
 sp3.text(20, 0.04, 'Fitted parameters: \np: %f \neps: %f' % (popt_ene_true[0], popt_ene_true[1]))
 
 
+M_cut = 10
 
-# furman_pivi_surface_tweaked = {'exclude_rediffused': False,
-#                                'choice': 'poisson',
-#                                'M_cut': 10,
-#                                'p_n': np.array([2.5, 3.3, 2.5, 2.5, 2.8, 1.3, 1.5, 1.5, 1.5, 1.5]),
-#                                'eps_n': np.array([1.5, 1.75, 1., 3.75, 8.5, 11.5, 2.5, 3., 2.5, 3.]),
-#                                # Parameters for backscattered electrons
-#                                'p1EInf': 0.002158,  # Changed this
-#                                'p1Ehat': 0.709633,  # Changed this
-#                                'eEHat': 0.,
-#                                'w': 46.028959,  # Changed this
-#                                'p': 0.468907,  # Changed this
-#                                'e1': 0.,  # Changed this
-#                                'e2': 2.,
-#                                'sigmaE': 2.,
-#                                # Parameters for rediffused electrons
-#                                'p1RInf': 0.2,
-#                                'eR': 0.041,
-#                                'r': 0.104,
-#                                'q': 0.5,
-#                                'r1': 0.26,
-#                                'r2': 2.,
-#                                # Parameters for true secondaries
-#                                'deltaTSHat': 1.8848,
-#                                'eHat0': 332.,
-#                                's': 1.35,
-#                                't1': 0.727814,  # Changed this
-#                                't2': 0.69333,  # Changed this
-#                                't3': 0.7,
-#                                't4': 1.,
-#                                }
-#
-# sey_mod = fp.SEY_model_furman_pivi(E_th=35., sigmafit=1.0828, mufit=1.6636, secondary_angle_distribution='cosine_3D',
-#                                    switch_no_increase_energy=0, thresh_low_energy=-1,
-#                                    furman_pivi_surface=furman_pivi_surface_tweaked)
 
+def true_sec_energy_PDF(delta_ts, nn, E_0, energy, p_n, eps_n):
+    """The PDF for true secondary electrons."""
+    if nn == 0:
+        raise ValueError('nn = 0, you cannot emit zero electrons.')
+
+    nn_all = np.arange(0, M_cut + 1, 1)
+
+    P_n_ts = np.squeeze(delta_ts**nn_all / factorial(nn_all) * np.exp(-delta_ts))
+
+    P_n_ts = P_n_ts / np.sum(P_n_ts)
+    P_n_ts_return = P_n_ts[int(nn)]
+    eps_curr = eps_n[int(nn - 1)]
+    p_n_curr = p_n[int(nn - 1)]
+
+    if E_0 == 0:
+        F_n = 0
+    else:
+        F_n = 1
+    f_n_ts = F_n * energy**(p_n_curr - 1) * np.exp(-energy / eps_curr)
+    area = scipy.integrate.simps(f_n_ts, energy)
+    f_n_ts = f_n_ts / area  # normalisation
+
+    return f_n_ts, P_n_ts_return
+
+
+def average_true_sec_energy_PDF(delta_ts, E_0, energy, p_n, eps_n):
+    nns = np.arange(1, M_cut + 1, 1)
+    average_f_n_ts = np.zeros_like(energy)
+    for ii in nns:
+        f_n_ts, P_n_ts = true_sec_energy_PDF(delta_ts=delta_ts, nn=ii, E_0=E_0, energy=energy, p_n=p_n, eps_n=eps_n)
+        average_f_n_ts = average_f_n_ts + f_n_ts * P_n_ts * ii
+    area = scipy.integrate.simps(average_f_n_ts, energy)
+    return average_f_n_ts / area
+
+
+energy = np.linspace(0.0000001, 35., num=int(1e3))
+ene_FP = pdf_elas_FP(energy, E_imp=35., sigma_e=.01)
+
+popt_ene_elas, pcov_ene_elas = curve_fit(pdf_elas_FP, energy, ene_FP, p0=[1., 1.])
+fitted_ene_elas = pdf_elas_FP(energy, *popt_ene_elas)
+
+sp4 = fig.add_subplot(2, 2, 4)
+sp4.plot(energy, ene_FP, color='k', label='ECLOUD', linewidth=linewid)
+sp4.plot(energy, fitted_ene_elas, '--', color='r', label='Furman-Pivi', linewidth=linewid)
+sp4.legend()
+sp4.set_ylabel('Energy distribution', fontsize=fontsz)
+sp4.set_xlabel('Emitted electron energy [eV]', fontsize=fontsz)
+sp4.grid(alpha=alph)
+sp4.text(20, 0.04, 'Fitted parameters: \nE_0: %f \n$\sigma_e$: %f' % (popt_ene_elas[0], popt_ene_elas[1]))
 
 plt.show()
