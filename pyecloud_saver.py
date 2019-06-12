a6331566adfc8e7621b19402947f917513bef479
Lorenzo Giacomel <lorenzo.giacomel@cern.ch>
2019-06-12 15:02:48 +0200

    changed name to histogram saving routines

--- pyecloud_saver.py
+++ pyecloud_saver.py
@@ -92,8 +92,8 @@ class pyecloud_saver:
             git_branch = 'Retrieving git branch failed'
             print(e)
         print(git_branch)
-        
-        if self.logfile_path is not None: 
+
+        if self.logfile_path is not None:
             with open(self.logfile_path, 'w') as flog:
                 flog.write('PyECLOUD Version 7.7.1\n')
                 flog.write('%s\n' % git_hash)
@@ -161,7 +161,7 @@ class pyecloud_saver:
 
         # Init step by step data saving
         self._stepbystep_data_init(Dt_ref, dec_fact_out, el_density_probes, r_center,
-                                   initial_size_t_vect=1000, 
+                                   initial_size_t_vect=1000,
                                    step_by_step_custom_observables = self.step_by_step_custom_observables)
 
         # Init pass by pass data saving
@@ -169,7 +169,7 @@ class pyecloud_saver:
                                      x_min_hist_det, x_max_hist_det, y_min_hist_det, y_max_hist_det, Dx_hist_det)
 
         # Init energy and cos angle histogram saving
-        self._energy_and_cos_angle_hist_init(Dt_En_hist, flag_cos_angle_hist, cos_angle_width, flag_lifetime_hist, Dt_lifetime_hist)
+        self._energy_cos_and_lifetime_angle_hist_init(Dt_En_hist, flag_cos_angle_hist, cos_angle_width, flag_lifetime_hist, Dt_lifetime_hist)
 
 
         #Space charge electrostatic energy
@@ -239,7 +239,7 @@ class pyecloud_saver:
         self._sc_video_save(spacech_ele, beamtim)
 
         # Check for energy and cos angle hist update
-        self._energy_and_cos_angle_hist_save(beamtim, impact_man)
+        self._energy_cos_and_lifetime_angle_hist_save(beamtim, impact_man, MP_e)
 
         #Space charge electrostatic energy
         if spacech_ele.last_recomputation_check:
@@ -326,7 +326,7 @@ class pyecloud_saver:
 
             self.nel_hist_det_line = np.zeros(self.Nxg_hist_det, float)
             self.nel_hist_det = []
-        
+
         # Custom data
         self.pbp_custom_data = {}
         if self.pass_by_pass_custom_observables is not None:
@@ -788,7 +788,7 @@ class pyecloud_saver:
 
         for kk in self.sbs_custom_data.keys():
             dict_sbs_data[kk] = self.sbs_custom_data[kk][:self.i_last_save + 1]
-        
+
         return dict_sbs_data
 
     def _MP_state_init(self, save_mp_state_time_file):
@@ -866,7 +866,7 @@ class pyecloud_saver:
                 'flag_multiple_clouds': self.flag_multiple_clouds,
                 'cloud_list': cloud_list,
                 't_last_En_hist': self.t_last_En_hist}
-            
+
             if self.flag_lifetime_hist:
                 dict_state['t_last_lifetime_hist'] = self.t_last_lifetime_hist
 
@@ -987,7 +987,7 @@ class pyecloud_saver:
             timestr = time.strftime("%d %b %Y %H:%M:%S", time.localtime())
 
             string_tolog = timestr + (' pass. %d/%d, cloud=%s: Nel_tot=%e N_mp=%d\n'%(beamtim.pass_numb, beamtim.N_pass_tot, self.cloud_name, np.sum(MP_e.nel_mp[0:MP_e.N_mp]), MP_e.N_mp))
-            
+
             if self.logfile_path is not None:
                 try:
                     with open(self.logfile_path, 'a') as flog:
@@ -996,7 +996,7 @@ class pyecloud_saver:
                     print('Got: ', err)
                     print('while trying to write the following line on logfile:')
                     print(string_tolog)
-            
+
             if self.progress_path is not None:
                 try:
                     with open(self.progress_path, 'w') as flog:
@@ -1014,7 +1014,7 @@ class pyecloud_saver:
             except IOError:
                 pass
 
-    def _energy_and_cos_angle_hist_init(self, Dt_En_hist, flag_cos_angle_hist,
+    def _energy_cos_and_lifetime_angle_hist_init(self, Dt_En_hist, flag_cos_angle_hist,
                                         cos_angle_width, flag_lifetime_hist, Dt_lifetime_hist):
         # Energy histogram init
         self.Dt_En_hist = Dt_En_hist
@@ -1043,7 +1043,7 @@ class pyecloud_saver:
         # Kinetic energy histogram init
         self.all_Ekin_hist = []
 
-    def _energy_and_cos_angle_hist_save(self, beamtim, impact_man, MP_e):
+    def _energy_cos_and_lifetime_angle_hist_save(self, beamtim, impact_man, MP_e):
         # Energy histogram saver
         # if (np.mod(beamtim.ii_curr,self.Nst_En_hist)==0):
         if beamtim.tt_curr >= self.t_last_En_hist + self.Dt_En_hist or np.isclose(beamtim.tt_curr, self.t_last_En_hist + self.Dt_En_hist, rtol=1.e-10, atol=0.0):
