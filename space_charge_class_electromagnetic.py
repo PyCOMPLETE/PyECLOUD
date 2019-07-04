new attempt to fix the time derivatives

space_charge_class_electromagnetic.py
@@ -76,6 +76,9 @@ class space_charge_electromagnetic(space_charge, object):
         self.beta = np.sqrt(1-1/(gamma*gamma))
 
     def recompute_spchg_emfield(self, MP_e, flag_solve=True, flag_reset=True):
+        #update the old states before scattering 
+        self.state_Ax_old = self.state_Ax.get_state_object()
+        self.state_Ay_old = self.state_Ay.get_state_object()
         # scatter rho
         self.PyPICobj.scatter(MP_e.x_mp[0:MP_e.N_mp], MP_e.y_mp[0:MP_e.N_mp], MP_e.nel_mp[0:MP_e.N_mp],
                 charge=MP_e.charge, flag_add=not(flag_reset))
@@ -95,8 +98,6 @@ class space_charge_electromagnetic(space_charge, object):
         if flag_solve:
             self.PyPICobj.solve()
             self.PyPICobj.solve_states([self.state_Ax, self.state_Ay, self.state_As])
-
-
 
     def get_sc_em_field(self, MP_e):
         #compute un-primed potentials (with wrong sign)
@@ -138,8 +139,6 @@ class space_charge_electromagnetic(space_charge, object):
             dAx_dt = np.zeros(MP_e.N_mp)
             dAy_dt = np.zeros(MP_e.N_mp)
 
-        self.state_Ax_old = self.state_Ax.get_state_object()
-        self.state_Ay_old = self.state_Ay.get_state_object()
         #compute B-field in  boosted frame
         Bx_prime = dAs_prime_dy + 1/(self.beta*c)*dAy_dt
         By_prime = -1/(self.beta*c)*dAx_dt - dAs_prime_dx