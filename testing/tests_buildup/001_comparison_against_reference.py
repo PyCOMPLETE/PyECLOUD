c6a9d264792dcee06a47374b72456c82c8f2b598
lmether <lotta.mether@cern.ch>
2019-07-19 11:52:09 +0200

    Restore default simulation also in the plot script.

--- testing/tests_buildup/001_comparison_against_reference.py
+++ testing/tests_buildup/001_comparison_against_reference.py
@@ -13,7 +13,7 @@ import PyECLOUD.mystyle as ms
 
 pl.close('all')
 
-#sim_folder = 'LHC_ArcDipReal_450GeV_sey1.70_2.5e11ppb_bl_1.00ns'
+sim_folder = 'LHC_ArcDipReal_450GeV_sey1.70_2.5e11ppb_bl_1.00ns'
 #sim_folder = 'LHC_ArcDipReal_450GeV_sey1.00_2.5e11ppb_bl_1.00ns_gas_ionization'
 #sim_folder = 'LHC_ArcDipReal_450GeV_sey1.70_2.5e11ppb_bl_1.00ns_change_s_and_E0'
 #sim_folder = 'LHC_ArcDipReal_450GeV_sey1.70_2.5e11ppb_bl_1.00ns_multigrid'
@@ -36,7 +36,7 @@ pl.close('all')
 #sim_folder = 'LHC_ArcDipReal_450GeV_sey1.70_2.5e11ppb_bl_1.00ns_stress_saver'
 #sim_folder = 'LHC_ArcDipReal_450GeV_sey1.70_2.5e11ppb_bl_1.00ns_nonuniftime'
 #sim_folder = 'LHC_ArcDipReal_450GeV_sey1.70_2.5e11ppb_bl_1.00ns_checkpoint/'
-sim_folder = 'Rectangular_Dip_450GeV_sey1.60_1.1e11ppb_furman_pivi'
+#sim_folder = 'Rectangular_Dip_450GeV_sey1.60_1.1e11ppb_furman_pivi'
 
 # check if user provided folder as command line argument
 parser = argparse.ArgumentParser()
