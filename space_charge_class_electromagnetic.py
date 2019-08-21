86b8c808ebcc51ebc2da90e44d7394bda2437ca3
Lorenzo Giacomel <lorenzo.giacomel@cern.ch>
2019-08-21 10:45:49 +0200

    added tests for comparisons of magnetic terms of the force on the beam

--- space_charge_class_electromagnetic.py
+++ space_charge_class_electromagnetic.py
@@ -62,6 +62,8 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import rhocompute as rhocom
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib import colors
+import int_field_for as iff
 
 na = lambda x: np.array([x])
 
@@ -143,12 +145,12 @@ class space_charge_electromagnetic(space_charge, object):
         By_sc_n = -1/(self.beta*c_light)*dAx_dt
         Bz_sc_n = dAy_dx - dAx_dy
 
-        if self.ii%50 == 0:
-            ########################################################################
+        if self.ii%20 == 0:
+            #######################################################################
             dphi_dx = -m_dphi_dx
             dphi_dy = -m_dphi_dy
-            self.compare_terms(dphi_dx, dphi_dy, dAx_dt, dAy_dt, Bx_sc_n, By_sc_n, self.beta, self.gamma, MP_e)
-            ########################################################################
+            self.compare_terms3(dphi_dx, dphi_dy, dAx_dt, dAy_dt, Bx_sc_n, By_sc_n, self.beta, self.gamma, MP_e)
+            #######################################################################
         self.ii = self.ii+1
         return Ex_sc_n, Ey_sc_n, Bx_sc_n, By_sc_n, Bz_sc_n
 
@@ -168,47 +170,59 @@ class space_charge_electromagnetic(space_charge, object):
         mpl.rc('xtick', labelsize=fontsz)
         mpl.rc('ytick', labelsize=fontsz)
         fontsz = 20
+        plt.set_cmap('bwr')
 
         plt.subplot(2,3,3)
-        plt.imshow(dphi_dx.T, cmap=None, norm=None, aspect='auto', interpolation=None,
-                 alpha=None, origin='lower', extent=[xmin * 1e3, xmax * 1e3, ymin * 1e3, ymax * 1e3])
-        clb = plt.colorbar(format='%.0e')
+        plt.imshow(dphi_dx.T, cmap=None, aspect='auto', interpolation=None,
+                 alpha=None, origin='lower', extent=[xmin * 1e3, xmax * 1e3, ymin * 1e3, ymax * 1e3], norm=MidpointNormalize(midpoint=0))
+        clb = plt.colorbar()
+        clb.formatter.set_powerlimits((0, 0))
+        clb.update_ticks()
         clb.set_label(r'$\left[\frac{V}{m}\right]$',fontsize = fontsz)
         plt.title(r'$\frac{\partial \phi}{\partial x}$',fontsize = fontsz)
         plt.subplot(2,3,6)
-        plt.imshow(dphi_dy.T, cmap=None, norm=None, aspect='auto', interpolation=None,
+        plt.imshow(dphi_dy.T, cmap=None, norm=MidpointNormalize(midpoint=0), aspect='auto', interpolation=None,
                  alpha=None, origin='lower', extent=[xmin * 1e3, xmax * 1e3, ymin * 1e3, ymax * 1e3])
         plt.title(r'$\frac{\partial \phi}{\partial y}$',fontsize = fontsz)
-        clb = plt.colorbar(format='%.0e')
-
+        clb = plt.colorbar()
+        clb.formatter.set_powerlimits((0, 0))
+        clb.update_ticks()
         clb.set_label(r'$\left[\frac{V}{m}\right]$',fontsize = fontsz)
         plt.subplot(2,3,1)
         ax = plt.gca()
-        plt.imshow(dAx_dt.T, cmap=None, norm=None, aspect='auto', interpolation=None,
-                alpha=None, origin='lower', extent=[xmin * 1e3, xmax * 1e3, ymin * 1e3, ymax * 1e3])
+        plt.imshow(dAx_dt.T, cmap=None, aspect='auto', interpolation=None,
+                alpha=None, origin='lower', extent=[xmin * 1e3, xmax * 1e3, ymin * 1e3, ymax * 1e3], norm=MidpointNormalize(midpoint=0))
         plt.title(r'$\frac{\partial A_x}{\partial t}$',fontsize = fontsz)
-        clb = plt.colorbar(format='%.0e')
+        clb = plt.colorbar()
+        clb.formatter.set_powerlimits((0, 0))
+        clb.update_ticks()
         clb.set_label(r'$\left[\frac{V}{m}\right]$',fontsize = fontsz)
         plt.subplot(2,3,2)
-        plt.imshow(-beta*c_light*By_sc_n.T, cmap=None, norm=None, aspect='auto', interpolation=None,
+        plt.imshow(-beta*c_light*By_sc_n.T, cmap=None, norm=MidpointNormalize(midpoint=0), aspect='auto', interpolation=None,
                  alpha=None, origin='lower', extent=[xmin * 1e3, xmax * 1e3, ymin * 1e3, ymax * 1e3])
         plt.title(r'$-\beta*c*B_y$',fontsize = fontsz-5)
-        clb = plt.colorbar(format='%.0e')
+        clb = plt.colorbar()
+        clb.formatter.set_powerlimits((0, 0))
+        clb.update_ticks()
         clb.set_label(r'$\left[\frac{V}{m}\right]$',fontsize = fontsz)
         plt.subplot(2,3,4)
-        plt.imshow(dAy_dt.T, cmap=None, norm=None, aspect='auto', interpolation=None,
+        plt.imshow(dAy_dt.T, cmap=None, norm=MidpointNormalize(midpoint=0), aspect='auto', interpolation=None,
                  alpha=None, origin='lower', extent=[xmin * 1e3, xmax * 1e3, ymin * 1e3, ymax * 1e3])
         plt.title(r'$\frac{\partial A_y}{\partial t}$',fontsize = fontsz)
-        clb = plt.colorbar(format='%.0e')
+        clb = plt.colorbar()
+        clb.formatter.set_powerlimits((0, 0))
+        clb.update_ticks()
         clb.set_label(r'$\left[\frac{V}{m}\right]$',fontsize = fontsz)
         plt.subplot(2,3,5)
-        plt.imshow(beta*c_light*Bx_sc_n.T, cmap=None, norm=None, aspect='auto', interpolation=None,
+        plt.imshow(beta*c_light*Bx_sc_n.T, cmap=None, norm=MidpointNormalize(midpoint=0), aspect='auto', interpolation=None,
                  alpha=None, origin='lower', extent=[xmin * 1e3, xmax * 1e3, ymin * 1e3, ymax * 1e3])
         plt.title(r'$\beta*c*B_x$',fontsize = fontsz-5)
-        clb = plt.colorbar(format='%.0e')
+        clb = plt.colorbar()
+        clb.formatter.set_powerlimits((0, 0))
+        clb.update_ticks()
         clb.set_label(r'$\left[\frac{V}{m}\right]$',fontsize = fontsz)
         #plt.ylim(min(dAx_dt),max(dAx_dt))
-        plt.subplots_adjust(bottom=0.1, right=0.9, left = 0.05, top=0.9, hspace=0.5, wspace=0.8)
+        plt.subplots_adjust(bottom=0.1, right=0.93, left = 0.05, top=0.9, hspace=0.5, wspace=0.5)
         #plt.draw()
         #plt.pause(1e-5)
         filename = str('comparison/%05d' % (self.ii)) + '.png'
@@ -219,9 +233,140 @@ class space_charge_electromagnetic(space_charge, object):
         #print('dphi_dx:%f'%(dphi_dx[10]))
         #print('err: %.16f'%(np.sqrt(np.mean(np.square(dAx_dt+beta*c_light*By_sc_n)))))
 
+
+    def compare_terms2(self, dphi_dx_mp, dphi_dy_mp, dAx_dt_mp, dAy_dt_mp, Bx_sc_n_mp, By_sc_n_mp, beta, gamma, MP_e):
+        grad_phi_norm = np.sqrt(np.square(dphi_dx_mp)+np.square(dphi_dy_mp))
+        dA_dt_norm = np.sqrt(np.square(dAx_dt_mp)+np.square(dAy_dt_mp))
+        B_term_norm = beta*c_light*np.sqrt(np.square(Bx_sc_n_mp)+np.square(By_sc_n_mp))
+        #plt.plot(dphi_dx[inds],'b')
+        fontsz = 15
+        mpl.rc('xtick', labelsize=fontsz)
+        mpl.rc('ytick', labelsize=fontsz)
+
+        fontsz = 20
+        plt.set_cmap('bwr')
+
+        win = 50
+
+        plt.subplot(1,3,1)
+        hist, bins = np.histogram(dA_dt_norm, bins = 1000)
+        hist = np.true_divide(hist,sum(hist))
+        hist = np.convolve(hist, np.ones((win,))/win, mode='same')
+        plt.plot(bins[:-1],hist)
+        plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+        plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        plt.xlabel(r'$||\frac{d\mathbf{A}}{dt}||$')
+        plt.subplot(1,3,2)
+        hist, bins = np.histogram(B_term_norm, bins = 1000)
+        hist = np.true_divide(hist,sum(hist))
+        hist = np.convolve(hist, np.ones((win,))/win, mode='same')
+        plt.plot(bins[:-1],hist)
+        plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+        plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        plt.xlabel(r'$||\beta c \hat{\mathbf{i}}_s \times \mathbf{A}||$')
+        plt.subplot(1,3,3)
+        hist, bins = np.histogram(grad_phi_norm, bins = 1000)
+        hist = np.true_divide(hist,sum(hist))
+        hist = np.convolve(hist, np.ones((win,))/win, mode='same')
+        plt.plot(bins[:-1],hist)
+        plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+        plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        plt.xlabel(r'$||\nabla \phi||$')
+        #plt.ylim(min(dAx_dt),max(dAx_dt))
+        plt.subplots_adjust(bottom=0.1, right=0.93, left = 0.05, top=0.9, hspace=0.5, wspace=0.5)
+        #plt.draw()
+        #plt.pause(1e-5)
+        filename = str('comparison_hists/%05d' % (self.ii)) + '.png'
+        plt.savefig(filename, dpi=150)
+        plt.clf()
+        #print('dAx_dt:%f'%(dAx_dt[10]))
+        #print('-beta*c*By_sc_n:%f'%(-beta*c_light*By_sc_n[10]))
+        #print('dphi_dx:%f'%(dphi_dx[10]))
+        #print('err: %.16f'%(np.sqrt(np.mean(np.square(dAx_dt+beta*c_light*By_sc_n)))))
+
+    def compare_terms3(self, dphi_dx_mp, dphi_dy_mp, dAx_dt_mp, dAy_dt_mp, Bx_sc_n_mp, By_sc_n_mp, beta, gamma, MP_e):
+        grad_phi_norm_mp = np.sqrt(np.square(dphi_dx_mp)+np.square(dphi_dy_mp))
+        dA_dt_norm_mp = np.sqrt(np.square(dAx_dt_mp)+np.square(dAy_dt_mp))
+        B_term_norm_mp = beta*c_light*np.sqrt(np.square(Bx_sc_n_mp)+np.square(By_sc_n_mp))
+        scale=10000000
+        grad_phi_norm_grid = rhocom.compute_sc_rho(MP_e.x_mp[0:MP_e.N_mp],MP_e.y_mp[0:MP_e.N_mp],grad_phi_norm_mp,self.bias_x,self.bias_y,self.Dh,self.Nxg,self.Nyg)
+        dA_dt_norm_grid = rhocom.compute_sc_rho(MP_e.x_mp[0:MP_e.N_mp],MP_e.y_mp[0:MP_e.N_mp],dA_dt_norm_mp,self.bias_x,self.bias_y,self.Dh,self.Nxg,self.Nyg)
+        B_term_norm_grid = rhocom.compute_sc_rho(MP_e.x_mp[0:MP_e.N_mp],MP_e.y_mp[0:MP_e.N_mp],B_term_norm_mp,self.bias_x,self.bias_y,self.Dh,self.Nxg,self.Nyg)
+        sxx=0.0006656379575134914
+        syy=0.0006849358075842791
+        sxy=0
+        #S = np.matrix([[sxx,sxy],[sxy,syy]])
+        #m = [0,0]
+        #XY_mp = np.random.multivariate_normal(m,S,1000)
+        #X_mp = XY_mp[:,0]
+        #Y_mp = XY_mp[:,1]
+        #rho = np.linspace(0,3*sxx,1000)
+        #theta = np.linspace(0,2*np.pi,1000)
+        #X_mp = np.multiply(rho,np.cos(theta))
+        #Y_mp = np.multiply(rho,np.sin(theta))
+        xx = np.linspace(-3*sxx,3*sxx,100)
+        yy = np.linspace(-3*syy,3*syy,100)
+        X_mp = []
+        Y_mp = []
+        for i in range(len(xx)):
+            for j in range(len(xx)):
+                if xx[i]*xx[i]/(9*sxx*sxx)+yy[j]*yy[j]/(9*syy*syy)<=1:
+                    X_mp.append(xx[i])
+                    Y_mp.append(yy[j])
+
+        grad_phi_norm = iff.int_field(X_mp,Y_mp,self.bias_x,self.bias_y,self.Dh,self.Dh,grad_phi_norm_grid,grad_phi_norm_grid)
+        dA_dt_norm = iff.int_field(X_mp,Y_mp,self.bias_x,self.bias_y,self.Dh,self.Dh,dA_dt_norm_grid,dA_dt_norm_grid)
+        B_term_norm = iff.int_field(X_mp,Y_mp,self.bias_x,self.bias_y,self.Dh,self.Dh,B_term_norm_grid,B_term_norm_grid)
+
+        #plt.plot(dphi_dx[inds],'b')
+        fontsz = 15
+        mpl.rc('xtick', labelsize=fontsz)
+        mpl.rc('ytick', labelsize=fontsz)
+
+        fontsz = 20
+        plt.set_cmap('bwr')
+
+        win = 50
+
+        #plt.subplot(1,3,1)
+        hist, bins = np.histogram(dA_dt_norm, bins = 1000)
+        hist = np.true_divide(hist,sum(hist))
+        hist = np.convolve(hist, np.ones((win,))/win, mode='same')
+        plt.semilogx(bins[:-1],hist,'b',label=r'$||\frac{d\mathbf{A}}{dt}||$')
+        #plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+        #plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        #plt.xlabel(r'$||\frac{d\mathbf{A}}{dt}||$', fontsize=fontsz)
+        #plt.subplot(1,3,2)
+        hist, bins = np.histogram(B_term_norm, bins = 1000)
+        hist = np.true_divide(hist,sum(hist))
+        hist = np.convolve(hist, np.ones((win,))/win, mode='same')
+        plt.semilogx(bins[:-1],hist,'r', linestyle='--', dashes=(5, 10), label=r'$||\beta c \hat{\mathbf{i}}_s \times \mathbf{A}||$')
+        #plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+        #plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        #plt.xlabel(r'$||\beta c \hat{\mathbf{i}}_s \times \mathbf{A}||$', fontsize=fontsz)
+        #plt.subplot(1,3,3)
+        hist, bins = np.histogram(grad_phi_norm, bins = 1000)
+        hist = np.true_divide(hist,sum(hist))
+        hist = np.convolve(hist, np.ones((win,))/win, mode='same')
+        plt.semilogx(bins[:-1],hist,'g',label=r'$||\nabla \phi||$')
+        #plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+        #plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        #plt.xlabel(r'$||\nabla \phi||$', fontsize=fontsz)
+        #plt.ylim(min(dAx_dt),max(dAx_dt))
+        plt.legend(fontsize=fontsz)
+        plt.xlabel(r'$[N]$', fontsize=fontsz)
+        #plt.subplots_adjust(bottom=0.15, right=0.93, left = 0.05, top=0.95, hspace=0.5, wspace=0.5)
+        #plt.draw()
+        #plt.pause(1e-5)
+        filename = str('comparison_hists/%05d' % (self.ii)) + '.png'
+        plt.savefig(filename, dpi=150)
+        plt.clf()
+
     '''
     def compare_terms(self, dphi_dx, dphi_dy, dAx_dt, dAy_dt, Bx_sc_n, By_sc_n, beta, gamma):
-        indsx = sorted(range(len(dAx_dt)), key=lambda k: dAx_dt[k])
+        indsx = sorted(range(leIf False, the result will contain the number of samples in each bin. If True, the result is the value of the probability density function at the bin, normalized such that the integral over the range is 1. Note that the sum of the histogram values will not be equal to 1 unless bins of unity width are chosen; it is not a probability mass function.
+
+n(dAx_dt)), key=lambda k: dAx_dt[k])
         indsy = sorted(range(len(dAy_dt)), key=lambda k: dAy_dt[k])
         indsphix = sorted(range(len(dphi_dx)), key=lambda k: dphi_dx[k])
         indsphiy = sorted(range(len(dphi_dy)), key=lambda k: dphi_dy[k])
@@ -255,3 +400,15 @@ class space_charge_electromagnetic(space_charge, object):
         #print('dphi_dx:%f'%(dphi_dx[10]))
         #print('err: %.16f'%(np.sqrt(np.mean(np.square(dAx_dt+beta*c_light*By_sc_n)))))
     '''
+
+# set the colormap and centre the colorbar
+class MidpointNormalize(colors.Normalize):
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
