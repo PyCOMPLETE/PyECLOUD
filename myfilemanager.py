b6af6e0f9773f1212632c549f33d09b76de375d7
giadarol <giovanni.iadarola@cern.ch>
2019-11-19 18:59:08 +0100

    Removed tabs

--- myfilemanager.py
+++ myfilemanager.py
@@ -5,15 +5,14 @@ class obj_from_dict:
     def __init__(self, dictto):
         for kk in dictto.keys():
             setattr(self, kk, dictto[kk])
-            
-            
+
+
 def obj_to_dict(obj):
     dict_out={}
     members = dir(obj)
     for member in members:
         dict_out[member] = getattr(obj, member)
     return dict_out
-    
 
 def myloadmat(filename, squeeze = True):
     import scipy.io as sio
@@ -25,12 +24,10 @@ def myloadmat(filename, squeeze = True):
             except:
                 pass
     return dict_var
-            
-            
+
 def myloadmat_to_obj(filename, squeeze = True):
     return  obj_from_dict(myloadmat(filename, squeeze=squeeze))
-    
-    
+
 def dict_of_arrays_and_scalar_from_h5(filename):
     import h5py
     with h5py.File(filename, 'r') as fid:
@@ -40,7 +37,7 @@ def dict_of_arrays_and_scalar_from_h5(filename):
             if f_dict[kk].shape == ():
                 f_dict[kk] = f_dict[kk].tolist()
     return  f_dict
-    
+
 def object_with_arrays_and_scalar_from_h5(filename):
     return  obj_from_dict(dict_of_arrays_and_scalar_from_h5(filename))
 
@@ -52,20 +49,20 @@ def monitorh5_to_dict(filename, key= 'Bunch'):
         monitor_dict = {}
         for kk in monitor.keys():
             monitor_dict[kk] = np.array(monitor[kk]).copy()
-        
+
     return monitor_dict
-    
+
 
 def monitorh5_to_obj(filename, key= 'Bunch'):
     return  obj_from_dict(monitorh5_to_dict(filename, key))
-    
+
 def monitorh5list_to_dict(filename_list, key='Bunch', flag_transpose=False, permissive=False):
     monitor_dict = monitorh5_to_dict(filename_list[0], key=key)
     for i_file in xrange(1, len(filename_list)):
         print('Loading '+filename_list[i_file])
         try:
             monitor_dict_curr = monitorh5_to_dict(filename_list[i_file], key=key)
-	    if flag_transpose:
+            if flag_transpose:
                 for kk in monitor_dict.keys():
                     monitor_dict[kk] = np.array(list(monitor_dict[kk].T)+list(monitor_dict_curr[kk].T)).T
             else:
@@ -76,7 +73,7 @@ def monitorh5list_to_dict(filename_list, key='Bunch', flag_transpose=False, perm
             print(err)
             if not permissive:
                 raise err
-    
+
     return monitor_dict
 
 def monitorh5list_to_obj(filename_list, key= 'Bunch', flag_transpose=False, permissive=False):
@@ -87,7 +84,7 @@ def dict_to_h5(dict_save, filename, compression=None, compression_opts=None):
     import h5py
     with h5py.File(filename, 'w') as fid:
         for kk in dict_save.keys():
-	    fid.create_dataset(kk, data=dict_save[kk],
+            fid.create_dataset(kk, data=dict_save[kk],
 		compression=compression, compression_opts=compression_opts)
 
 
