5939aa46a455f1ebe38cfde7b484d843ba5e39fa
giadarol <giovanni.iadarola@cern.ch>
2020-01-27 17:35:36 +0100

    Some style changes

--- other/follow_electrons/001_analyze_trajectories.py
+++ other/follow_electrons/001_analyze_trajectories.py
@@ -2,11 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import PyECLOUD.myfilemanager as mfm
-
+import PyECLOUD.mystyle as ms
 obfol = mfm.myloadmat_to_obj('followed_electrons.mat')
 ob = mfm.myloadmat_to_obj('Pyecltest.mat')
 
 i_obs = 1
+
+plt.close('all')
+ms.mystyle(fontsz=14, traditional_look=False)
 
 Dz_all = []
 nel_all = []
@@ -28,7 +31,6 @@ for i_ele in range(obfol.nel.shape[1]):
             nel_all.append(nel_ele[i_changes[ii]+1])
 
     if i_ele == i_obs:
-        plt.close('all')
         fig1 = plt.figure(1)
         ax1 = fig1.add_subplot(2,1,1)
         ax2 = fig1.add_subplot(2,1,2, sharex=ax1)
@@ -41,5 +43,12 @@ hist, bin_edges = np.histogram(Dz_all, bins=100, range=(0, 0.4), weights=nel_all
 
 fig2 = plt.figure(2)
 ax21 = fig2.add_subplot(1,1,1)
-ax21.plot(0.5*(bin_edges[1:]+bin_edges[:-1]), hist)
+#ax21.plot(0.5*(bin_edges[1:]+bin_edges[:-1]), hist)
+ax21.bar(x=0.5*(bin_edges[1:]+bin_edges[:-1]), height=hist,
+        align='center', width=np.mean(np.diff(bin_edges)))
+ax21.set_xlim(left=0.)
+ax21.set_ylim(bottom=0.)
+ax21.set_ylabel('Occurrencies')
+ax21.set_xlabel('Longitudinal displacement [m]')
+ax21.grid(True, linestyle=':', alpha=.5)
 plt.show()
