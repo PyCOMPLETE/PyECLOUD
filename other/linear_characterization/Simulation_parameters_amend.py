8f5feb20c379b2aa9af0f43eb9f3289dfd49a05f
giadarol <giovanni.iadarola@cern.ch>
2020-07-08 10:16:28 +0200

    Characterization of quadrupolar

---
+++ other/linear_characterization/Simulation_parameters_amend.py
@@ -0,0 +1,8 @@
+force_interp_at_substeps_interacting_slices = True
+Dt_ref = 10e-12
+
+pyecl_input_folder = '!folder_of_this_file!/reference_simulation/pyecloud_config'
+
+enable_transverse_damper = False
+
+n_slices = 200
